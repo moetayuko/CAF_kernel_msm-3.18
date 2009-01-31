ef1b1bd98244521983dffe04fecc621ef7647761
Dima Zavin <dima@android.com>
2009-01-30 18:54:12 -0800

    [ARM] msm: swordfish: enable nand flash.
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/board-swordfish.c
+++ arch/arm/mach-msm/board-swordfish.c
@@ -55,6 +55,7 @@ static struct platform_device smc91x_device = {
 static struct platform_device *devices[] __initdata = {
 	&msm_device_uart3,
 	&msm_device_smd,
+	&msm_device_nand,
 	&smc91x_device,
 };
 
