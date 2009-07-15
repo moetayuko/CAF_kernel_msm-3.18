1c7bdb87db2b4f565293156cd0d5b52710836aff
Dima Zavin <dima@android.com>
2009-07-14 18:24:16 -0700

    HACK: [ARM] msm: mahamahi: Make the panel directly talk to the msm spi controller HACK
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/board-mahimahi-panel.c
+++ arch/arm/mach-msm/board-mahimahi-panel.c
@@ -14,6 +14,7 @@
  *
  */
 
+#include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/init.h>
@@ -24,20 +25,58 @@
 #include <asm/mach-types.h>
 
 #include <mach/msm_fb.h>
+#include <mach/msm_iomap.h>
 
 #include "board-mahimahi.h"
 #include "devices.h"
 
+
+#define SPI_CONFIG              (0x00000000)
+#define SPI_IO_CONTROL          (0x00000004)
+#define SPI_OPERATIONAL         (0x00000030)
+#define SPI_ERROR_FLAGS_EN      (0x00000038)
+#define SPI_ERROR_FLAGS         (0x00000038)
+#define SPI_OUTPUT_FIFO         (0x00000100)
+
+static void __iomem *spi_base;
+static struct clk *spi_clk ;
+
 #define CLK_NS_TO_RATE(ns)			(1000000000UL / (ns))
-#define lcm_writeb(x...)			do {} while (0)
+
+static int qspi_send(uint32_t id, uint8_t data)
+{
+	uint32_t err;
+
+	/* bit-5: OUTPUT_FIFO_NOT_EMPTY */
+	while (readl(spi_base + SPI_OPERATIONAL) & (1<<5)) {
+		if ((err = readl(spi_base + SPI_ERROR_FLAGS))) {
+			pr_err("%s: ERROR: SPI_ERROR_FLAGS=0x%08x\n", __func__,
+			       err);
+			return -EIO;
+		}
+	}
+	writel((0x7000 | (id << 9) | data) << 16, spi_base + SPI_OUTPUT_FIFO);
+	udelay(100);
+
+	return 0;
+}
+
+static int lcm_writeb(uint8_t reg, uint8_t val)
+{
+	qspi_send(0x0, reg);
+	qspi_send(0x1, val);
+	return 0;
+}
 
 static int samsung_oled_panel_blank(struct msm_lcdc_panel_ops *ops)
 {
 	pr_info("%s: +()\n", __func__);
+	clk_enable(spi_clk);
 	lcm_writeb(0x14, 0x1);
 	msleep(200);
 	lcm_writeb(0x1d, 0xa1);
 	msleep(300);
+	clk_disable(spi_clk);
 	pr_info("%s: -()\n", __func__);
 	return 0;
 }
@@ -45,10 +84,12 @@ static int samsung_oled_panel_blank(struct msm_lcdc_panel_ops *ops)
 static int samsung_oled_panel_unblank(struct msm_lcdc_panel_ops *ops)
 {
 	pr_info("%s: +()\n", __func__);
+	clk_enable(spi_clk);
 	lcm_writeb(0x1d, 0xa0);
 	msleep(200);
 	lcm_writeb(0x14, 0x03);
 	msleep(100);
+	clk_disable(spi_clk);
 	pr_info("%s: -()\n", __func__);
 	return 0;
 }
@@ -90,6 +131,7 @@ static int samsung_oled_panel_init(struct msm_lcdc_panel_ops *ops)
 	int i;
 
 	pr_info("%s: +()\n", __func__);
+	clk_enable(spi_clk);
 	for (i = 0; i< ARRAY_SIZE(samsung_oled_init_table); i++) {
 		lcm_writeb(samsung_oled_init_table[i].reg,
 			   samsung_oled_init_table[i].val);
@@ -98,6 +140,7 @@ static int samsung_oled_panel_init(struct msm_lcdc_panel_ops *ops)
 	}
 
 	lcm_writeb(0x14, 0x3);
+	clk_disable(spi_clk);
 	pr_info("%s: -()\n", __func__);
 
 	return 0;
@@ -112,11 +155,11 @@ static struct msm_lcdc_panel_ops mahimahi_lcdc_panel_ops = {
 static struct msm_lcdc_timing mahimahi_lcdc_timing = {
 		.clk_rate		= CLK_NS_TO_RATE(26),
 		.hsync_pulse_width	= 4,
-		.hsync_back_porch	= 4,
+		.hsync_back_porch	= 8,
 		.hsync_front_porch	= 8,
 		.hsync_skew		= 0,
 		.vsync_pulse_width	= 2,
-		.vsync_back_porch	= 6,
+		.vsync_back_porch	= 8,
 		.vsync_front_porch	= 8,
 		.vsync_act_low		= 1,
 		.hsync_act_low		= 1,
@@ -147,6 +190,39 @@ static struct platform_device mahimahi_lcdc_device = {
 	},
 };
 
+static int mahimahi_init_spi_hack(void)
+{
+	int ret;
+
+	spi_base = ioremap(MSM_SPI_PHYS, MSM_SPI_SIZE);
+	if (!spi_base)
+		return -1;
+
+	spi_clk = clk_get(&msm_device_spi.dev, "spi_clk");
+	if (IS_ERR(spi_clk)) {
+		pr_err("%s: unable to get spi_clk\n", __func__);
+		ret = PTR_ERR(spi_clk);
+		goto err_clk_get;
+	}
+
+	clk_enable(spi_clk);
+
+	printk("spi: SPI_CONFIG=%x\n", readl(spi_base + SPI_CONFIG));
+	printk("spi: SPI_IO_CONTROL=%x\n", readl(spi_base + SPI_IO_CONTROL));
+	printk("spi: SPI_OPERATIONAL=%x\n", readl(spi_base + SPI_OPERATIONAL));
+	printk("spi: SPI_ERROR_FLAGS_EN=%x\n",
+	       readl(spi_base + SPI_ERROR_FLAGS_EN));
+	printk("spi: SPI_ERROR_FLAGS=%x\n", readl(spi_base + SPI_ERROR_FLAGS));
+	printk("-%s()\n", __FUNCTION__);
+	clk_disable(spi_clk);
+
+	return 0;
+
+err_clk_get:
+	iounmap(spi_base);
+	return ret;
+}
+
 int __init mahimahi_init_panel(void)
 {
 	int ret;
@@ -155,6 +231,10 @@ int __init mahimahi_init_panel(void)
 		return 0;
 
 	ret = platform_device_register(&msm_device_mdp);
+	if (ret != 0)
+		return ret;
+
+	ret = mahimahi_init_spi_hack();
 	if (ret != 0)
 		return ret;
 
