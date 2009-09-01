0868a085b6a89a4083ec2790215df05f45ed9cf5
Dima Zavin <dima@android.com>
2009-08-31 21:28:15 -0700

    [ARM] msm: swordfish: Add ADSP pmem region (32MB)
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/board-swordfish.c
+++ arch/arm/mach-msm/board-swordfish.c
@@ -178,11 +178,27 @@ static struct android_pmem_platform_data android_pmem_gpu1_pdata = {
         .cached         = 0,
 };
 
+static struct android_pmem_platform_data android_pmem_adsp_pdata = {
+        .name           = "pmem_adsp",
+        .start          = MSM_PMEM_ADSP_BASE,
+        .size           = MSM_PMEM_ADSP_SIZE,
+        .no_allocator   = 0,
+        .cached         = 0,
+};
+
 static struct platform_device android_pmem_mdp_device = {
         .name           = "android_pmem",
         .id             = 0,
         .dev            = {
                 .platform_data = &mdp_pmem_pdata
+        },
+};
+
+static struct platform_device android_pmem_adsp_device = {
+        .name           = "android_pmem",
+        .id             = 1,
+        .dev            = {
+                .platform_data = &android_pmem_adsp_pdata,
         },
 };
 
@@ -255,6 +271,7 @@ static struct platform_device *devices[] __initdata = {
 	&smc91x_device,
 	&msm_device_touchscreen,
 	&android_pmem_mdp_device,
+	&android_pmem_adsp_device,
 	&android_pmem_gpu0_device,
 	&android_pmem_gpu1_device,
 	&msm_kgsl_device,
