1b2e56adfdda4df40ddd728f944b8c830fd076db
Iliyan Malchev <malchev@google.com>
2009-09-13 18:47:04 -0700

    [ARM] msm: camera: msm8k updates
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -41,7 +41,6 @@ struct msm_camera_device_platform_data {
 	struct msm_camera_io_ext ioext;
 };
 
-#ifdef CONFIG_SENSORS_MT9T013
 struct msm_camera_legacy_device_platform_data {
 	int sensor_reset;
 	int sensor_pwd;
@@ -49,7 +48,6 @@ struct msm_camera_legacy_device_platform_data {
 	void (*config_gpio_on) (void);
 	void (*config_gpio_off)(void);
 };
-#endif
 
 #define MSM_CAMERA_FLASH_NONE 0
 #define MSM_CAMERA_FLASH_LED  1
@@ -61,7 +59,10 @@ struct msm_camera_sensor_info {
 	int vcm_pwd;
 	int mclk;
 	int flash_type;
+	int need_suspend;
 	struct msm_camera_device_platform_data *pdata;
+	struct resource *resource;
+	uint8_t num_resources;
 };
 
 struct snd_endpoint {
