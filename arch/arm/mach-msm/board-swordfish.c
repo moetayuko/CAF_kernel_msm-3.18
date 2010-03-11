487877808c69c8013eb1621d949886102ff2939d
Abhijeet Dharmapurikar <adharmap@quicinc.com>
2010-03-11 13:29:13 -0800

    msm: swordfish: fix the definition of msm_serial_debug_init
    The UART_RX irq on swordfish happens on GPIO 86. Define
    wakeup_irq to be GPIO 86.
    
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@quicinc.com>
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>

--- arch/arm/mach-msm/board-swordfish.c
+++ arch/arm/mach-msm/board-swordfish.c
@@ -303,7 +303,7 @@ static struct msm_acpu_clock_platform_data swordfish_clock_data = {
 };
 
 void msm_serial_debug_init(unsigned int base, int irq,
-			   struct device *clk_device, int signal_irq);
+			   struct device *clk_device, int signal_irq, int wakeup_irq);
 
 static void __init swordfish_init(void)
 {
@@ -312,7 +312,8 @@ static void __init swordfish_init(void)
 	msm_acpu_clock_init(&swordfish_clock_data);
 #if defined(CONFIG_MSM_SERIAL_DEBUGGER)
 	msm_serial_debug_init(MSM_UART3_PHYS, INT_UART3,
-			      &msm_device_uart3.dev, 1);
+			      &msm_device_uart3.dev, 1,
+			      MSM_GPIO_TO_INT(86));
 #endif
 	msm_device_hsusb.dev.platform_data = &msm_hsusb_pdata;
 	msm_device_touchscreen.dev.platform_data = &swordfish_ts_pdata;
