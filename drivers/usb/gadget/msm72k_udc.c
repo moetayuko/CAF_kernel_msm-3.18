5c1c36de7bf69ff968f3afb9cbd68410e6a27e59
Mike Lockwood <lockwood@android.com>
2010-03-23 20:11:00 -0400

    [ARM] msm: msm7k_udc: Remove dead request tracking logic.
    This code was broken as there was no safe way to handle requests being freed
    from completion routines.  It was also unnecessary because the gadget framework
    does not allow freeing busy requests anyway.
    This fixes a race condition that could result in a use after free bug when
    requests are freed via a completion routine.
    
    This change also fixes updating msm_endpoint "last" pointer in msm72k_dequeue().
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- drivers/usb/gadget/msm72k_udc.c
+++ drivers/usb/gadget/msm72k_udc.c
@@ -83,7 +83,6 @@ struct msm_request {
 	unsigned busy:1;
 	unsigned live:1;
 	unsigned alloced:1;
-	unsigned dead:1;
 
 	dma_addr_t dma;
 	dma_addr_t item_dma;
@@ -339,16 +338,6 @@ fail2:
 fail1:
 	return 0;
 }
-
-static void do_free_req(struct usb_info *ui, struct msm_request *req)
-{
-	if (req->alloced)
-		kfree(req->req.buf);
-
-	dma_pool_free(ui->pool, req->item, req->item_dma);
-	kfree(req);
-}
-
 
 static void usb_ept_enable(struct msm_endpoint *ept, int yes,
 		unsigned char ep_type)
@@ -801,9 +790,6 @@ static void handle_endpoint(struct usb_info *ui, unsigned bit)
 			req->req.complete(&ept->ep, &req->req);
 			spin_lock_irqsave(&ui->lock, flags);
 		}
-
-		if (req->dead)
-			do_free_req(ui, req);
 	}
 	spin_unlock_irqrestore(&ui->lock, flags);
 }
@@ -876,8 +862,6 @@ static void flush_endpoint_sw(struct msm_endpoint *ept)
 			req->req.complete(&ept->ep, &req->req);
 			spin_lock_irqsave(&ui->lock, flags);
 		}
-		if (req->dead)
-			do_free_req(ui, req);
 		req = req->next;
 	}
 	spin_unlock_irqrestore(&ui->lock, flags);
@@ -1496,18 +1480,16 @@ msm72k_free_request(struct usb_ep *_ep, struct usb_request *_req)
 	struct msm_request *req = to_msm_request(_req);
 	struct msm_endpoint *ept = to_msm_endpoint(_ep);
 	struct usb_info *ui = ept->ui;
-	unsigned long flags;
-	int dead = 0;
 
-	spin_lock_irqsave(&ui->lock, flags);
-	/* defer freeing resources if request is still busy */
+	/* request should not be busy */
+	BUG_ON(req->busy);
 	if (req->busy)
-		dead = req->dead = 1;
-	spin_unlock_irqrestore(&ui->lock, flags);
+		return;
 
-	/* if req->dead, then we will clean up when the request finishes */
-	if (!dead)
-		do_free_req(ui, req);
+	if (req->alloced)
+		kfree(req->req.buf);
+	dma_pool_free(ui->pool, req->item, req->item_dma);
+	kfree(req);
 }
 
 static int
@@ -1563,14 +1545,13 @@ static int msm72k_dequeue(struct usb_ep *_ep, struct usb_request *_req)
 				req->req.complete(&ep->ep, &req->req);
 				spin_lock_irqsave(&ui->lock, flags);
 			}
-			if (req->dead)
-				do_free_req(ui, req);
 			/* remove from linked list */
 			if (prev)
 				prev->next = cur->next;
 			else
 				ep->req = cur->next;
-			prev = cur;
+			if (ep->last == cur)
+				ep->last = prev;
 			/* break from loop */
 			cur = NULL;
 		} else
