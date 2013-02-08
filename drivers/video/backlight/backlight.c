7edc2bd6b4ec710d03c8a49b0fbe70e43df7153f
Derek Basehore <dbasehore@chromium.org>
2013-02-07 20:22:38 -0800

    CHROMIUM: dark resume: Add dark resume functionality
    In drivers/base/power/main.c, create the concept of dark resume. This includes
    checking for a dark resume before we resume devices and allowing devices to
    query this state. It also adds creating wake sources that indicate if a dark
    resume happened. If a device is in this list of devices, we ask it if it woke
    the system up. If so, we count the system resume as a dark resume.
    
    BUG=chrome-os-partner:9812
    TEST=suspend system, watch dark resumes happen
    
    Change-Id: I0c416c156759728284901f03e98f0c87933c9c9a
    [sonnyrao: 3.8: make dpm_dark_list static to match others]
    [sonnyrao: 3.8: fixup trivial conflict in pm.h]
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/42921
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    
    Conflicts:
    	include/linux/pm.h
    	drivers/usb/core/usb.c
    	drivers/leds/led-class.c
    
    [rebase412(groeck): Resolved conflicts;
    	Squashed:
    	CHROMIUM: dark resume: Expose dark resume status to user space
    	CHROMIUM: dark resume: Expose dark resume activation
    	CHROMIUM: dark resume: Make usb devices not resume for dark resumes
    	CHROMIUM: Separate dark resume into its own sysfs attr group
    	CHROMIUM: Check dark resume state earlier
    	CHROMIUM: dark resume: move to general platform api
    	CHROMIUM: dark resume: fix memory leak
    	CHROMIUM: power: Clear dark_resume_state when using pm_test
    	CHROMIUM: usb: change how dark resume works
    	CHROMIUM: Add sysfs entry for always waking up in dark resume
    	CHROMIUM: usb: don't resume root hub async in dark resume
    	CHROMIUM: leds: Adds dark resume capability
    	CHROMIUM: dark resume: split consumer and source functions
    	]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -15,6 +15,7 @@
 #include <linux/ctype.h>
 #include <linux/err.h>
 #include <linux/fb.h>
+#include <linux/pm_dark_resume.h>
 #include <linux/slab.h>
 
 #ifdef CONFIG_PMAC_BACKLIGHT
@@ -261,6 +262,11 @@ static int backlight_resume(struct device *dev)
 {
 	struct backlight_device *bd = to_backlight_device(dev);
 
+	if (dev_dark_resume_active(dev)) {
+		dev_info(dev, "disabled for dark resume\n");
+		return 0;
+	}
+
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops && bd->ops->options & BL_CORE_SUSPENDRESUME) {
 		bd->props.state &= ~BL_CORE_SUSPENDED;
@@ -369,6 +375,7 @@ struct backlight_device *backlight_device_register(const char *name,
 		return ERR_PTR(rc);
 	}
 
+	dev_dark_resume_add_consumer(&new_bd->dev);
 	new_bd->ops = ops;
 
 #ifdef CONFIG_PMAC_BACKLIGHT
@@ -437,6 +444,7 @@ void backlight_device_unregister(struct backlight_device *bd)
 	mutex_unlock(&bd->ops_lock);
 
 	backlight_unregister_fb(bd);
+	dev_dark_resume_remove_consumer(&bd->dev);
 	device_unregister(&bd->dev);
 }
 EXPORT_SYMBOL(backlight_device_unregister);
