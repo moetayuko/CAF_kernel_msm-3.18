3f7123135b39d1854dca7c424c237c6169bcdc80
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2013-03-28 22:01:51 +0100

    Add 2 warnings to allow the build to succeed with gcc 4.8
    gcc 4.8 creates some additional, apparently harmless, warnings,
    causing the build to fail unless they're allowed here.
    
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -43,6 +43,8 @@ allowed_warnings = set([
     "alignment.c:327",
     "mmu.c:602",
     "return_address.c:62",
+    "transport.c:653", # 'midQ' may be used uninitialized in this function with gcc >= 4.8
+    "bapRsnTxRx.c:222", # 'pPacket' may be used uninitialized in this function with gcc >= 4.8
  ])
 
 # Capture the name of the object file, can find it.
