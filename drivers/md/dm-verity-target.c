d411967ff1f185badf6dd51a1b80d82919d656b9
Paul Taysom <taysom@chromium.org>
2013-04-10 10:03:40 -0700

    CHROMIUM: md: dm-verity fixed setting error_behavior
    Need to be able to set the error behavior from either the device table
    or from the linux command line.
    
    The code only handled setting from the linux command line which
    broke the platform_DMVetiryCorruption test. This was not noticed
    earlier because of another bug in error handling that was just
    recently was fixed which exposed this problem.
    
    Now the code defaults to the command line but overrides from
    the device table.
    
    BUG=chromium:229621
    TEST=platform_DMVerityCorruption
    Signed-off-by: Paul Taysom <taysom@chromium.org>
    
    Change-Id: I97731782db26c013dae948e2ad53a7ade2bab152
    Reviewed-on: https://gerrit.chromium.org/gerrit/47744
    Tested-by: Paul Taysom <taysom@chromium.org>
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>
    Commit-Queue: Paul Taysom <taysom@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/51367
    Reviewed-by: Paul Taysom <taysom@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    
    [rebase44(briannorris): Fix conflicts when squashing in 4.4 fixups to
    earlier patches]
    Signed-off-by: Brian Norris <briannorris@chromium.org>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -1049,7 +1049,7 @@ static void pr_args(struct verity_args *args)
 	printk(KERN_INFO "VERITY args: version=%d data_device=%s hash_device=%s"
 		" data_block_size_bits=%d hash_block_size_bits=%d"
 		" num_data_blocks=%lld hash_start_block=%lld"
-		" algorithm=%s digest=%s salt=%s\n",
+		" algorithm=%s digest=%s salt=%s error_behavior=%s\n",
 		args->version,
 		args->data_device,
 		args->hash_device,
@@ -1059,7 +1059,8 @@ static void pr_args(struct verity_args *args)
 		args->hash_start_block,
 		args->algorithm,
 		args->digest,
-		args->salt);
+		args->salt,
+		args->error_behavior);
 }
 
 /*
@@ -1215,6 +1216,7 @@ int verity_ctr(struct dm_target *ti, unsigned argc, char **argv)
 	int i;
 	sector_t hash_position;
 
+	args.error_behavior = error_behavior;
 	if (argc >= DM_VERITY_NUM_POSITIONAL_ARGS)
 		ti->error = positional_args(argc, argv, &args);
 	else
@@ -1402,10 +1404,9 @@ int verity_ctr(struct dm_target *ti, unsigned argc, char **argv)
 				       __alignof__(struct dm_verity_io));
 
 	/* chromeos allows setting error_behavior from both the module
-	 * parameters and the device args. However, chromeos only uses
-	 * the module parameters
+	 * parameters and the device args.
 	 */
-	v->error_behavior = verity_parse_error_behavior(error_behavior);
+	v->error_behavior = verity_parse_error_behavior(args.error_behavior);
 	if (v->error_behavior == -1) {
 		ti->error = "Bad error_behavior supplied";
 		r = -EINVAL;
