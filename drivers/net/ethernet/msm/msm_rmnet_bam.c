31f70615742b74364dfdb69e936d4ac98300fb65
Harout Hedeshian <harouth@codeaurora.org>
2013-07-12 07:30:57 -0600

    rmnet_bam: Free netdevice resources if init fails
    In the init routine, after the netdevice is allocated,
    there are failure cases, where the netdevice was not
    being unregistered and free'd. This patch handles all the
    failure cases in the init routine and does the necessary
    clean up.
    
    CRs-Fixed: 490419
    Change-Id: I71b2318e61dffa1e2dca0e835ec6d964679b2639
    Signed-off-by: Harout Hedeshian <harouth@codeaurora.org>

--- drivers/net/ethernet/msm/msm_rmnet_bam.c
+++ drivers/net/ethernet/msm/msm_rmnet_bam.c
@@ -899,6 +899,7 @@ static int __init rmnet_init(void)
 		if (ret) {
 			pr_err("%s: unable to register netdev"
 				   " %d rc=%d\n", __func__, n, ret);
+			netdevs[n] = NULL;
 			free_netdev(dev);
 			return ret;
 		}
@@ -922,8 +923,11 @@ static int __init rmnet_init(void)
 		bam_rmnet_drivers[n].probe = bam_rmnet_probe;
 		bam_rmnet_drivers[n].remove = bam_rmnet_remove;
 		tempname = kmalloc(BAM_DMUX_CH_NAME_MAX_LEN, GFP_KERNEL);
-		if (tempname == NULL)
-			return -ENOMEM;
+		if (tempname == NULL) {
+			netdevs[n] = NULL;
+			ret = -ENOMEM;
+			goto error;
+		}
 		scnprintf(tempname, BAM_DMUX_CH_NAME_MAX_LEN, "bam_dmux_ch_%d",
 									n);
 		bam_rmnet_drivers[n].driver.name = tempname;
@@ -932,7 +936,8 @@ static int __init rmnet_init(void)
 		if (ret) {
 			pr_err("%s: registration failed n=%d rc=%d\n",
 					__func__, n, ret);
-			return ret;
+			netdevs[n] = NULL;
+			goto error;
 		}
 	}
 	/*Support for new rmnet ports */
@@ -961,6 +966,7 @@ static int __init rmnet_init(void)
 		if (ret) {
 			pr_err("%s: unable to register rev netdev %d rc=%d\n",
 							__func__, n, ret);
+			netdevs_rev[n] = NULL;
 			free_netdev(dev);
 			return ret;
 		}
@@ -969,8 +975,11 @@ static int __init rmnet_init(void)
 		bam_rmnet_rev_drivers[n].probe = bam_rmnet_rev_probe;
 		bam_rmnet_rev_drivers[n].remove = bam_rmnet_rev_remove;
 		tempname = kmalloc(BAM_DMUX_CH_NAME_MAX_LEN, GFP_KERNEL);
-		if (tempname == NULL)
-			return -ENOMEM;
+		if (tempname == NULL) {
+			netdevs_rev[n] = NULL;
+			ret = -ENOMEM;
+			goto error;
+		}
 		scnprintf(tempname, BAM_DMUX_CH_NAME_MAX_LEN, "bam_dmux_ch_%d",
 					(n+BAM_DMUX_DATA_REV_RMNET_0));
 		bam_rmnet_rev_drivers[n].driver.name = tempname;
@@ -979,10 +988,16 @@ static int __init rmnet_init(void)
 		if (ret) {
 			pr_err("%s: new rev driver registration failed n=%d rc=%d\n",
 					__func__, n, ret);
-			return ret;
+			netdevs_rev[n] = NULL;
+			goto error;
 		}
 	}
 	return 0;
+
+error:
+	unregister_netdev(dev);
+	free_netdev(dev);
+	return ret;
 }
 
 module_init(rmnet_init);
