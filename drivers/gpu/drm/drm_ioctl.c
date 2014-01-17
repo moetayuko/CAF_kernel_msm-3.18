9fd297794cd2147385fc5ffdfdea0b4f8664bc08
Nicolas Boichat <nicolas@boichat.ch>
2014-01-17 11:29:25 +0800

    CHROMIUM: drm: Add drm_master_relax debugfs file (non-root set/drop master ioctls)
    Writing 'Y' to /sys/kernel/debug/dri/drm_master_relax allows
    non-CAP_SYS_ADMIN user to call the 2 ioctls DRM_IOCTL_SET_MASTER and
    DRM_IOCTL_DROP_MASTER.
    
    This makes it possible to run 2 X11 servers at the same time, where at
    least one of them runs under a non-root user (e.g. Chromium OS X server
    as xorg user, and a crouton X server as root).
    
    The behavior is not modified if /sys/kernel/debug/dri/drm_master_relax
    is kept to its default value ('N').
    
    Modifying drm_master_relax is only possible by root user, so it cannot
    possibly lead to exploits. Also, even when the flag is set to 'Y',
    DROP_MASTER permissions are changed to DRM_MASTER which means that only
    a process that has DRM master can drop it (which is actually quite
    logical), and the SET_MASTER IOCTL is changed to DRM_AUTH which allows
    all authenticated DRM users to get master.
    
    R=seanpaul@chromium.org, dbehr@chromium.org
    BUG=chromium:328115
    TEST=With or without the debugfs flag, WebGL examples work, and we can
         switch between VT1 and VT2.
    TEST=On a peppy in developer mode: With the debugfs flag to 'Y', Ubuntu
         saucy chroot generated by crouton (xfce target) is able to start a
         second X11 display. Switching back and forth between the two
         displays works, and 3D acceleration is working in the chroot.
    
    Signed-off-by: Nicolas Boichat <nicolas@boichat.ch>
    Reviewed-on: https://chromium-review.googlesource.com/183190
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Commit-Queue: David Schneider <dnschneid@chromium.org>
    Tested-by: David Schneider <dnschneid@chromium.org>
    (cherry picked from commit d53b6142c2b5c742d2c547568c9814597c41ede6)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/189390
    Reviewed-by: Haixia Shi <hshi@chromium.org>
    
    [benzh: 3.14 rebase. Resolved trivial conflicts]
    Signed-off-by: Ben Zhang <benzh@chromium.org>
    [bfreed: 3.18 rebase. Resolved conflicts]
    Signed-off-by: Bryan Freed <bfreed@chromium.org>
    [briannorris: 4.4 rebase. Resolved conflicts; change drm_master_relax to
    'bool' to match usage and debugfs_create_bool() API change]
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    
    Change-Id: I7b8a00ca8e21321019875012a24cb88fea3c1317
    Reviewed-on: https://chromium-review.googlesource.com/267810
    Trybot-Ready: Bryan Freed <bfreed@chromium.org>
    Tested-by: Bryan Freed <bfreed@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Bryan Freed <bfreed@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/339283
    Commit-Queue: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    
    [groeck: 4.9 rebase: moved drm_master_relax declaration
     to drm_drv.h to match upstream reorganization]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -658,6 +658,7 @@ long drm_ioctl(struct file *filp,
 	char *kdata = NULL;
 	unsigned int in_size, out_size, drv_size, ksize;
 	bool is_driver_ioctl;
+	int flags;
 
 	dev = file_priv->minor->dev;
 
@@ -691,6 +692,15 @@ long drm_ioctl(struct file *filp,
 		  (long)old_encode_dev(file_priv->minor->kdev->devt),
 		  file_priv->authenticated, ioctl->name);
 
+	flags = ioctl->flags;
+	if (drm_master_relax) {
+		if (nr == DRM_IOCTL_NR(DRM_IOCTL_SET_MASTER))
+			flags = DRM_AUTH;
+		else if (nr == DRM_IOCTL_NR(DRM_IOCTL_DROP_MASTER))
+			flags = DRM_MASTER;
+	}
+
+
 	/* Do not trust userspace, use our own definition */
 	func = ioctl->func;
 
@@ -700,7 +710,7 @@ long drm_ioctl(struct file *filp,
 		goto err_i1;
 	}
 
-	retcode = drm_ioctl_permit(ioctl->flags, file_priv);
+	retcode = drm_ioctl_permit(flags, file_priv);
 	if (unlikely(retcode))
 		goto err_i1;
 
@@ -725,7 +735,7 @@ long drm_ioctl(struct file *filp,
 	/* Enforce sane locking for modern driver ioctls. Core ioctls are
 	 * too messy still. */
 	if ((!drm_core_check_feature(dev, DRIVER_LEGACY) && is_driver_ioctl) ||
-	    (ioctl->flags & DRM_UNLOCKED))
+	    (flags & DRM_UNLOCKED))
 		retcode = func(dev, kdata, file_priv);
 	else {
 		mutex_lock(&drm_global_mutex);
