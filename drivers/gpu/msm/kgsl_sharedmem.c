cb88103ad95b351b4e4c78576bb0658da90a0e05
Jordan Crouse <jcrouse@codeaurora.org>
2014-03-24 16:03:59 -0600

    msm: kgsl: Use a constant mask for the memops vmflags
    We don't need to define a function to return a constant.  Save
    ourselves some source code and some .text space too.
    
    Change-Id: Ic0dedbadc72c2fdd473cd4369ed772c84a923a15
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/kgsl_sharedmem.c

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -350,11 +350,6 @@ static int kgsl_page_alloc_vmfault(struct kgsl_memdesc *memdesc,
 	return VM_FAULT_SIGBUS;
 }
 
-static int kgsl_page_alloc_vmflags(struct kgsl_memdesc *memdesc)
-{
-	return VM_RESERVED | VM_DONTEXPAND;
-}
-
 static void kgsl_page_alloc_free(struct kgsl_memdesc *memdesc)
 {
 	int i = 0;
@@ -373,11 +368,6 @@ static void kgsl_page_alloc_free(struct kgsl_memdesc *memdesc)
 				break;
 			__free_pages(sg_page(sg), get_order(sg->length));
 		}
-}
-
-static int kgsl_contiguous_vmflags(struct kgsl_memdesc *memdesc)
-{
-	return VM_RESERVED | VM_IO | VM_PFNMAP | VM_DONTEXPAND;
 }
 
 /*
@@ -482,7 +472,8 @@ static void kgsl_coherent_free(struct kgsl_memdesc *memdesc)
 /* Global - also used by kgsl_drm.c */
 struct kgsl_memdesc_ops kgsl_page_alloc_ops = {
 	.free = kgsl_page_alloc_free,
-	.vmflags = kgsl_page_alloc_vmflags,
+	.vmflags = VM_IO | VM_DONTEXPAND,
+	//.vmflags = VM_RESERVED | VM_DONTEXPAND,
 	.vmfault = kgsl_page_alloc_vmfault,
 	.map_kernel_mem = kgsl_page_alloc_map_kernel,
 };
@@ -490,7 +481,8 @@ EXPORT_SYMBOL(kgsl_page_alloc_ops);
 
 static struct kgsl_memdesc_ops kgsl_ebimem_ops = {
 	.free = kgsl_ebimem_free,
-	.vmflags = kgsl_contiguous_vmflags,
+	.vmflags = VM_IO | VM_PFNMAP | VM_DONTEXPAND,
+	//.vmflags = VM_RESERVED | VM_PFNMAP | VM_DONTEXPAND,
 	.vmfault = kgsl_contiguous_vmfault,
 	.map_kernel_mem = kgsl_ebimem_map_kernel,
 };
