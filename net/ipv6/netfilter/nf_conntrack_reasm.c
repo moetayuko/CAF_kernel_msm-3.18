3fba1da557e1e6cf3b93bc4994e911bd676c0e66
Florian Westphal <fw@strlen.de>
2014-07-24 16:50:29 +0200

    inet: frag: constify match, hashfn and constructor arguments
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 36c7778218b93d96d88d68f116a711f6a598b72f)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- net/ipv6/netfilter/nf_conntrack_reasm.c
+++ net/ipv6/netfilter/nf_conntrack_reasm.c
@@ -144,7 +144,7 @@ static inline u8 ip6_frag_ecn(const struct ipv6hdr *ipv6h)
 	return 1 << (ipv6_get_dsfield(ipv6h) & INET_ECN_MASK);
 }
 
-static unsigned int nf_hashfn(struct inet_frag_queue *q)
+static unsigned int nf_hashfn(const struct inet_frag_queue *q)
 {
 	const struct frag_queue *nq;
 
