7489ddae8e32cb8fb800bbc76ba312c2ae0526ef
Florian Westphal <fw@strlen.de>
2014-07-24 16:50:30 +0200

    inet: frag: remove hash size assumptions from callers
    hide actual hash size from individual users: The _find
    function will now fold the given hash value into the required range.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit fb3cfe6e75b9d05c87265e85e67d7caf6e5b44a7)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
     Conflicts:
    	net/ipv6/netfilter/nf_conntrack_reasm.c
    	net/ipv6/reassembly.c

--- net/ipv6/netfilter/nf_conntrack_reasm.c
+++ net/ipv6/netfilter/nf_conntrack_reasm.c
@@ -144,6 +144,15 @@ static inline u8 ip6_frag_ecn(const struct ipv6hdr *ipv6h)
 	return 1 << (ipv6_get_dsfield(ipv6h) & INET_ECN_MASK);
 }
 
+static unsigned int nf_hash_frag(__be32 id, const struct in6_addr *saddr,
+				 const struct in6_addr *daddr)
+{
+	net_get_random_once(&nf_frags.rnd, sizeof(nf_frags.rnd));
+	return jhash_3words(ipv6_addr_hash(saddr), ipv6_addr_hash(daddr),
+			    (__force u32)id, nf_frags.rnd);
+}
+
+
 static unsigned int nf_hashfn(const struct inet_frag_queue *q)
 {
 	const struct frag_queue *nq;
