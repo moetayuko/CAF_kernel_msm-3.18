47fc5c6ba4fd1dda94e08803ffaea99264070871
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-11-17 14:42:00 +0100

    UPSTREAM: dmaengine: Introduce a device_config callback
    The fact that the channel configuration is done in device_control is rather
    misleading, since it's not really advertised as such, plus, the fact that the
    framework exposes a function of its own makes it not really intuitive, while
    we're losing the type checking whenever we pass that unsigned long argument.
    
    Add a device_config callback to dma_device, with a fallback on the old
    behaviour for now for existing drivers to opt in.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    (cherry picked from commit 94a73e30dfe6722e9f4ef19f7892901d7d00eab1)
    
    BUG=chrome-os-partner:49255
    TEST=Serial and DMA are working with these changes
    
    Change-Id: I07620a76564c9aa605d7750fe3db33274eae6794
    Signed-off-by: Matthew McClintock <mmcclint@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/332993
    Commit-Ready: Matthew McClintock <mmcclint@qca.qualcomm.com>
    Tested-by: Matthew McClintock <mmcclint@qca.qualcomm.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -607,6 +607,8 @@ struct dma_tx_state {
  *	The function takes a buffer of size buf_len. The callback function will
  *	be called after period_len bytes have been transferred.
  * @device_prep_interleaved_dma: Transfer expression in a generic way.
+ * @device_config: Pushes a new configuration to a channel, return 0 or an error
+ *	code
  * @device_control: manipulate all pending operations on a channel, returns
  *	zero or error code
  * @device_tx_status: poll for transaction completion, the optional
@@ -673,6 +675,9 @@ struct dma_device {
 	struct dma_async_tx_descriptor *(*device_prep_interleaved_dma)(
 		struct dma_chan *chan, struct dma_interleaved_template *xt,
 		unsigned long flags);
+
+	int (*device_config)(struct dma_chan *chan,
+			     struct dma_slave_config *config);
 	int (*device_control)(struct dma_chan *chan, enum dma_ctrl_cmd cmd,
 		unsigned long arg);
 
@@ -696,6 +701,9 @@ static inline int dmaengine_device_control(struct dma_chan *chan,
 static inline int dmaengine_slave_config(struct dma_chan *chan,
 					  struct dma_slave_config *config)
 {
+	if (chan->device->device_config)
+		return chan->device->device_config(chan, config);
+
 	return dmaengine_device_control(chan, DMA_SLAVE_CONFIG,
 			(unsigned long)config);
 }
