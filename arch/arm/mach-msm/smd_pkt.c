f217887605423b7be50f71656747a607fe1d2efa
Naveen Ramaraj <nramaraj@codeaurora.org>
2015-02-09 17:43:31 -0800

    msm: smd_pkt: Avoid prematurely stashing driver info
    smd_pkt_release() relies on checks that would pass by default
    (reference count == 0) to proceed with the release even if the
    open was unsuccessful in the first place
    
    Bug: b/18759663
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- arch/arm/mach-msm/smd_pkt.c
+++ arch/arm/mach-msm/smd_pkt.c
@@ -792,8 +792,6 @@ int smd_pkt_open(struct inode *inode, struct file *file)
 	}
 	D_STATUS("Begin %s on smd_pkt_dev id:%d\n", __func__, smd_pkt_devp->i);
 
-	file->private_data = smd_pkt_devp;
-
 	mutex_lock(&smd_pkt_devp->ch_lock);
 	if (smd_pkt_devp->ch == 0) {
 		INIT_COMPLETION(smd_pkt_devp->ch_allocated);
@@ -904,6 +902,8 @@ release_pd:
 		platform_driver_unregister(&smd_pkt_devp->driver);
 		smd_pkt_devp->driver.probe = NULL;
 	}
+	else if (r == 0)
+		file->private_data = smd_pkt_devp;
 out:
 	mutex_unlock(&smd_pkt_devp->ch_lock);
 
