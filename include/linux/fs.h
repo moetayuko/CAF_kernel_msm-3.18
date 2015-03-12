0bfaa1e63db77781e15b6dde7dbb2ee10a0aa1a3
Mohamad Ayyash <mkayyash@google.com>
2015-03-12 13:58:56 -0700

    Fix merge conflict in redef dir_emit

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1511,12 +1511,6 @@ struct dir_context {
 	loff_t pos;
 };
 
-static inline bool dir_emit(struct dir_context *ctx,
-			    const char *name, int namelen,
-			    u64 ino, unsigned type)
-{
-	return ctx->actor(ctx, name, namelen, ctx->pos, ino, type) == 0;
-}
 struct block_device_operations;
 
 /* These macros are for out of kernel modules to test that
