d6c894e515b4cd93c3a08e7c60cce0aa5118c656
Viresh Kumar <viresh.kumar@linaro.org>
2015-03-25 12:57:46 +0530

    hrtimer: make sure PINNED flag is cleared after removing hrtimer
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    [forward port to 3.18]
    Signed-off-by: Santosh Shukla <santosh.shukla@linaro.org>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -914,6 +914,7 @@ remove_hrtimer(struct hrtimer *timer, struct hrtimer_clock_base *base)
 		 */
 		state = timer->state & HRTIMER_STATE_CALLBACK;
 		__remove_hrtimer(timer, base, state, reprogram);
+		timer->state &= ~HRTIMER_STATE_PINNED;
 		return 1;
 	}
 	return 0;
@@ -955,7 +956,7 @@ int __hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim,
 
 	/* Update pinned state */
 	timer->state &= ~HRTIMER_STATE_PINNED;
-	timer->state |= !!(mode & HRTIMER_MODE_PINNED) << HRTIMER_PINNED_SHIFT;
+	timer->state |= (!!(mode & HRTIMER_MODE_PINNED)) << HRTIMER_PINNED_SHIFT;
 
 	leftmost = enqueue_hrtimer(timer, new_base);
 
