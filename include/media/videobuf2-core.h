2bf1be1cc4d203344b2683b48ff73d4f3cf57a80
Pawel Osciak <posciak@chromium.org>
2015-09-16 21:21:49 +0900

    CHROMIUM: vb2: Add a new "use_dma_bidirectional" queue flag.
    When set to 1 for CAPTURE queues by the driver on calling vb2_queue_init(),
    forces the buffers on the queue to be allocated/mapped with
    DMA_BIDIRECTIONAL DMA direction flag, instead of DMA_FROM_DEVICE. This
    allows the device not only to write to the buffers, but also read out from
    them. This may be useful e.g. for codec hardware, which may be using
    CAPTURE buffers as reference to decode other buffers.
    
    This flag is ignored for OUTPUT queues, as we don't want to allow HW to
    be able to write to OUTPUT buffers.
    
    Signed-off-by: Pawel Osciak <posciak@chromium.org>
    
    BUG=chrome-os-partner:45346
    TEST=video playback
    
    Change-Id: Ifba955eef75ac23c9a13edab04bc1fe7f5375c70
    Reviewed-on: https://chromium-review.googlesource.com/300726
    Commit-Ready: Pawel Osciak <posciak@chromium.org>
    Tested-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    
    Conflicts:
    	include/media/videobuf2-core.h
    
    [rebase412(groeck): Context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/media/videobuf2-core.h
+++ include/media/videobuf2-core.h
@@ -433,6 +433,9 @@ struct vb2_buf_ops {
  * @quirk_poll_must_check_waiting_for_buffers: Return POLLERR at poll when QBUF
  *              has not been called. This is a vb1 idiom that has been adopted
  *              also by vb2.
+ * @use_dma_bidirectional:	use DMA_BIDIRECTIONAL for CAPTURE buffers; this
+ *				allows HW to read from the CAPTURE buffers in
+ *				addition to writing; ignored for OUTPUT queues
  * @lock:	pointer to a mutex that protects the vb2_queue struct. The
  *		driver can set this to a mutex to let the v4l2 core serialize
  *		the queuing ioctls. If the driver wants to handle locking
@@ -500,6 +503,7 @@ struct vb2_queue {
 	unsigned			fileio_write_immediately:1;
 	unsigned			allow_zero_bytesused:1;
 	unsigned		   quirk_poll_must_check_waiting_for_buffers:1;
+	unsigned			use_dma_bidirectional:1;
 
 	struct mutex			*lock;
 	void				*owner;
