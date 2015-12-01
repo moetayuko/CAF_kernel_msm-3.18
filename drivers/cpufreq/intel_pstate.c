7859f05cb00bc3f98dcf733034aa30ffbb7021bf
Kristen Carlson Accardi <kristen@linux.intel.com>
2015-12-01 16:32:04 +0530

    BACKPORT: intel_pstate: enable HWP per CPU
    HWP previously was only enabled at driver load time, on the boot
    CPU, however, HWP must be enabled per package. Move the code to
    enable HWP to the cpufreq driver init path so that it will be
    called per CPU.
    
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>
    Tested-by: David Zhuang <david.zhuang@oracle.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit ba88d4338f226766f510e207911dde8c1875e072)
    Signed-off-by: Vishwanath Somayaji <vishwanath.somayaji@intel.com>
    Signed-off-by: Joe Konno <joe.konno@intel.com>
    
    Conflicts:
            drivers/cpufreq/intel_pstate.c
    
    BUG=chrome-os-partner:47517
    TEST=with the whole HWP series applied, check if HWP is enabled in dmesg and ftrace
    
    Change-Id: I1222bbbd0314c77c22a3c9ea1985c2b692efb016
    Reviewed-on: https://chromium-review.googlesource.com/320514
    Commit-Ready: Marc Herbert <marc.herbert@intel.com>
    Tested-by: Marc Herbert <marc.herbert@intel.com>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -475,12 +475,11 @@ static void __init intel_pstate_sysfs_expose_params(void)
 }
 /************************** sysfs end ************************/
 
-static void intel_pstate_hwp_enable(void)
+static void intel_pstate_hwp_enable(struct cpudata *cpudata)
 {
-	hwp_active++;
 	pr_info("intel_pstate HWP enabled\n");
 
-	wrmsrl( MSR_PM_ENABLE, 0x1);
+	wrmsrl_on_cpu(cpudata->cpu, MSR_PM_ENABLE, 0x1);
 }
 
 static int byt_get_min_pstate(void)
@@ -867,6 +866,10 @@ static int intel_pstate_init_cpu(unsigned int cpunum)
 	cpu = all_cpu_data[cpunum];
 
 	cpu->cpu = cpunum;
+
+	if (hwp_active)
+		intel_pstate_hwp_enable(cpu);
+
 	intel_pstate_get_cpu_pstates(cpu);
 
 	init_timer_deferrable(&cpu->timer);
@@ -1141,7 +1144,7 @@ static int __init intel_pstate_init(void)
 		return -ENOMEM;
 
 	if (cpu_has(c,X86_FEATURE_HWP) && !no_hwp)
-		intel_pstate_hwp_enable();
+		hwp_active++;
 
 	if (!hwp_active && hwp_only)
 		goto out;
