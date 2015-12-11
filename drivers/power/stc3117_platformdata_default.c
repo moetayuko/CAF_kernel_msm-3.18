b6b47d8ee3f639d8896d645af1073be31bc7ee79
st-sw <st-sw@users.noreply.github.com>
2015-12-11 00:01:03 -0800

    update init values

--- drivers/power/stc3117_platformdata_default.c
+++ drivers/power/stc3117_platformdata_default.c
@@ -18,45 +18,77 @@ static struct stc311x_platform_data stc3117_data = {
 	.power_supply_register = NULL,
 	.power_supply_unregister = NULL,
 
-	.Vmode= 0,       /*REG_MODE, BIT_VMODE 1=Voltage mode, 0=mixed mode */
+	//-------------------------------------------------------------------------
+	
+	//Battery specific, Mandatory:
+	.Cnom = 1500,       /* nominal battery capacity [in mAh] */
+	.Rint = 200,		/* nominal battery internal impedance [mOhms]*/
+	.CC_cnf = 303,      /* nominal CC_CNF (Coulomb mode Conf), computed from formula */
+	                    //REG_CC_CNF: (Rsense x Nominal battery capacity) / 49.556
+	.VM_cnf = 307,      /* nominal VM_CNF (Voltage Mode Conf), computed from formula*/
+	                    //REG_VM_CNF: (Internal battery impedance x Nominal battery capacity) / 977.78
+	
+	//Hardware specific, Mandatory:
+	.Rsense = 10,       /* sense resistor [mOhms]*/
+	
+	//-------------------------------------------------------------------------
+	
+	//Gas gauge specific, Default:
+	.Vmode= 0,       /* REG_MODE, BIT_VMODE 1=Voltage mode, 0=mixed mode */
 	.Alm_SOC = 10,      /* SOC alm level %*/
 	.Alm_Vbat = 3600,   /* Vbat alm level mV*/
-	.CC_cnf = 525,      /* nominal CC_cnf, coming from battery characterisation*/
-	.VM_cnf = 558,      /* nominal VM cnf , coming from battery characterisation*/
-	.Rint = 200,			/* nominal internal impedance*/
-	.Cnom = 2600,       /* nominal capacity in mAh, coming from battery characterisation*/
-	.Rsense = 10,       /* sense resistor mOhms*/
 	.RelaxCurrent = 150, /* current for relaxation in mA (< C/20) */
 	.Adaptive = 1,     /* 1=Adaptive mode enabled, 0=Adaptive mode disabled */
 
-	/* Elentec Co Ltd Battery pack - 80 means 8% */
-	.CapDerating[6] = 71,   /* capacity derating in 0.1%, for temp = -20°C */
-	.CapDerating[5] = 42,   /* capacity derating in 0.1%, for temp = -10°C */
-	.CapDerating[4] = 13,   /* capacity derating in 0.1%, for temp = 0°C */
-	.CapDerating[3] = 5,   /* capacity derating in 0.1%, for temp = 10°C */
-	.CapDerating[2] = 0,   /* capacity derating in 0.1%, for temp = 25°C */
-	.CapDerating[1] = 0,   /* capacity derating in 0.1%, for temp = 40°C */
-	.CapDerating[0] = 0,   /* capacity derating in 0.1%, for temp = 60°C */
+	//Battery specific, default:
+	.CapDerating[6] = 0,   /* capacity de-rating in 0.1%, for temp = -20°C */
+	.CapDerating[5] = 0,   /* capacity de-rating in 0.1%, for temp = -10°C */
+	.CapDerating[4] = 0,   /* capacity de-rating in 0.1%, for temp = 0°C */
+	.CapDerating[3] = 0,   /* capacity de-rating in 0.1%, for temp = 10°C */
+	.CapDerating[2] = 0,   /* capacity de-rating in 0.1%, for temp = 25°C */
+	.CapDerating[1] = 0,   /* capacity de-rating in 0.1%, for temp = 40°C */
+	.CapDerating[0] = 0,   /* capacity de-rating in 0.1%, for temp = 60°C */
 
-	.OCVValue[15] = 0,   /* OCV curve adjustment */
-	.OCVValue[14] = 0,   /* OCV curve adjustment */
-	.OCVValue[13] = 0,   /* OCV curve adjustment */
-	.OCVValue[12] = 0,   /* OCV curve adjustment */
-	.OCVValue[11] = 0,   /* OCV curve adjustment */
-	.OCVValue[10] = 0,   /* OCV curve adjustment */
-	.OCVValue[9] = 0,    /* OCV curve adjustment */
-	.OCVValue[8] = 0,    /* OCV curve adjustment */
-	.OCVValue[7] = 0,    /* OCV curve adjustment */
-	.OCVValue[6] = 0,    /* OCV curve adjustment */
-	.OCVValue[5] = 0,    /* OCV curve adjustment */
-	.OCVValue[4] = 0,    /* OCV curve adjustment */
-	.OCVValue[3] = 0,    /* OCV curve adjustment */
-	.OCVValue[2] = 0,    /* OCV curve adjustment */
-	.OCVValue[1] = 0,    /* OCV curve adjustment */
-	.OCVValue[0] = 0,    /* OCV curve adjustment */
+	.SOCValue[0] = 0x00,    /* SOC=0% - default SOC axis for battery OCV curve */
+	.SOCValue[1] = 0x01,    /* SOC=3% - default SOC axis for battery OCV curve */
+	.SOCValue[2] = 0x03,    /* SOC=6% - default SOC axis for battery OCV curve */
+	.SOCValue[3] = 0x05,    /* SOC=10% - default SOC axis for battery OCV curve */
+	.SOCValue[4] = 0x07,    /* SOC=15% - default SOC axis for battery OCV curve */
+	.SOCValue[5] = 0x0A,    /* SOC=20% - default SOC axis for battery OCV curve */
+	.SOCValue[6] = 0x0C,    /* SOC=25% - default SOC axis for battery OCV curve */
+	.SOCValue[7] = 0x0F,    /* SOC=30% - default SOC axis for battery OCV curve */
+	.SOCValue[8] = 0x14,    /* SOC=40% - default SOC axis for battery OCV curve */
+	.SOCValue[9] = 0x19,    /* SOC=50% - default SOC axis for battery OCV curve */
+	.SOCValue[10] = 0x1E,    /* SOC=60% - default SOC axis for battery OCV curve */
+	.SOCValue[11] = 0x20,    /* SOC=65% - default SOC axis for battery OCV curve */
+	.SOCValue[12] = 0x23,    /* SOC=70% - default SOC axis for battery OCV curve */
+	.SOCValue[13] = 0x28,    /* SOC=80% - default SOC axis for battery OCV curve */
+	.SOCValue[14] = 0x2D,    /* SOC=90% - default SOC axis for battery OCV curve */
+	.SOCValue[15] = 0x32,    /* SOC=100% - default SOC axis for battery OCV curve */
+	
+	.OCVValue[0] = 3300,    /* default battery OCV curve (at 0%) : OCV=3.3V */
+	.OCVValue[1] = 3541,    /* default battery OCV curve (at 3%) : OCV=3.54V */
+	.OCVValue[2] = 3618,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[3] = 3658,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[4] = 3695,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[5] = 3721,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[6] = 3747,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[7] = 3761,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[8] = 3778,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[9] = 3802,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[10]= 3863,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[11]= 3899,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[12]= 3929,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[13]= 3991,    /* default battery OCV curve (at x%) : OCV=...V */
+	.OCVValue[14]= 4076,    /* default battery OCV curve (at 90%) : OCV=4.076V */
+	.OCVValue[15]= 4176,    /* default battery OCV curve (at 100%) : OCV=4.176V */
 
-	/*if the application temperature data is preferred than the STC3117 temperature*/
-	.ExternalTemperature = Temperature_fn, /*External temperature fonction, return °C*/
+	
+	//-------------------------------------------------------------------------
+	//Platform specific:
+	
+	/* if the application temperature data is preferred than the STC3117 temperature */
+	.ExternalTemperature = Temperature_fn, /*External temperature function, return °C*/
 	.ForceExternalTemperature = 0, /* 1=External temperature, 0=STC3117 temperature */
 
 };
