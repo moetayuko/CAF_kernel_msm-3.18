33d4ed639c1d8e98bc02e93d0aff05114833f99d
Dylan Reid <dgreid@chromium.org>
2015-12-18 11:18:40 -0800

    CHROMIUM: net: check ns_capable(CAP_NET_RAW) in android additions
    Some locally carried patches add checks for capable(CAP_NET_RAW).  These
    predate user namespace support and cause the configuration of a network
    created in a user namepsace to break.  Replacing these with ns_capable
    calls restores the intended functionality.
    
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/319223
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    (cherry picked from commit 6082b56f6f036ae340ff41988077c8b57d90ea34)
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    [rebase44(drinkcat): inet6_killaddr_ioctl was removed, other minor
    context conflicts]
    
    BUG=b:29554103
    TEST=Build and boot on gru
    
    Change-Id: Icbd7ca4d0534a3a62b1290640a8440a5aaa46363
    Reviewed-on: https://chromium-review.googlesource.com/356862
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -68,12 +68,12 @@
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
 #include <linux/android_aid.h>
 
-static inline int current_has_network(void)
+static inline int current_has_network(struct net *net)
 {
-	return in_egroup_p(AID_INET) || capable(CAP_NET_RAW);
+	return in_egroup_p(AID_INET) || ns_capable(net->user_ns, CAP_NET_RAW);
 }
 #else
-static inline int current_has_network(void)
+static inline int current_has_network(struct net *net)
 {
 	return 1;
 }
@@ -135,7 +135,7 @@ static int inet6_create(struct net *net, struct socket *sock, int protocol,
 	if (protocol < 0 || protocol >= IPPROTO_MAX)
 		return -EINVAL;
 
-	if (!current_has_network())
+	if (!current_has_network(net))
 		return -EACCES;
 
 	/* Look for the requested type/protocol pair. */
@@ -184,7 +184,8 @@ lookup_protocol:
 	}
 
 	err = -EPERM;
-	if (sock->type == SOCK_RAW && !kern && !capable(CAP_NET_RAW))
+	if (sock->type == SOCK_RAW && !kern && !ns_capable(net->user_ns,
+							   CAP_NET_RAW))
 		goto out_rcu_unlock;
 
 	sock->ops = answer->ops;
