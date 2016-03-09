42fdca0347d8daf9cd8a2641717abdf96b20dfad
Mark Rutland <mark.rutland@arm.com>
2016-03-09 14:08:18 -0800

    UPSTREAM: sched/kasan: remove stale KASAN poison after hotplug
    Functions which the compiler has instrumented for KASAN place poison on
    the stack shadow upon entry and remove this poision prior to returning.
    
    In the case of CPU hotplug, CPUs exit the kernel a number of levels deep
    in C code.  Any instrumented functions on this critical path will leave
    portions of the stack shadow poisoned.
    
    When a CPU is subsequently brought back into the kernel via a different
    path, depending on stackframe, layout calls to instrumented functions
    may hit this stale poison, resulting in (spurious) KASAN splats to the
    console.
    
    To avoid this, clear any stale poison from the idle thread for a CPU
    prior to bringing a CPU online.
    
    BUG=chromium:583663
    TEST=run syzkaller on smaug with USE=kasan
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit e1b77c92981a522223bd1ac118fdcade6b7ad086)
    Signed-off-by: Alexander Potapenko <glider@google.com>
    
    Change-Id: I93cb5e9fd084320d7788ef713362e3391032413b
    Reviewed-on: https://chromium-review.googlesource.com/333901
    Commit-Ready: Alexander Potapenko <glider@chromium.org>
    Tested-by: Alexander Potapenko <glider@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -26,6 +26,7 @@
  *              Thomas Gleixner, Mike Kravetz
  */
 
+#include <linux/kasan.h>
 #include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/nmi.h>
@@ -4896,6 +4897,7 @@ void init_idle(struct task_struct *idle, int cpu)
 	idle->state = TASK_RUNNING;
 	idle->se.exec_start = sched_clock();
 
+	kasan_unpoison_task_stack(idle);
 	do_set_cpus_allowed(idle, cpumask_of(cpu));
 	/*
 	 * We're having a chicken and egg problem, even though we are
