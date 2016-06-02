4ea48732d4948d14eebe6b2f32226e477457761c
Stephen Rothwell <sfr@canb.auug.org.au>
2016-06-02 13:58:25 +1000

    pstore: update arch/powerpc/kernel/nvram_64.c for struct pstore_info change
    Fixes: d8aaf3511a13 ("pstore: add lzo/lz4 compression support")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/kernel/nvram_64.c
+++ arch/powerpc/kernel/nvram_64.c
@@ -444,7 +444,8 @@ static int nvram_pstore_write(enum pstore_type_id type,
  */
 static ssize_t nvram_pstore_read(u64 *id, enum pstore_type_id *type,
 				int *count, struct timespec *time, char **buf,
-				bool *compressed, struct pstore_info *psi)
+				bool *compressed, ssize_t *ecc_notice_size,
+				struct pstore_info *psi)
 {
 	struct oops_log_info *oops_hdr;
 	unsigned int err_type, id_no, size = 0;
