07d53959660465ca8f4ebefb3e5cbded1f476e2f
Gwendal Grignou <gwendal@chromium.org>
2016-06-30 17:15:36 -0700

    CHROMIUM: platform/chrome: Support MKBP protocol over ACPI
    Allow MKBP users to get events on ACPI based platform.
    This change requires corresponding change in Coreboot that adds a new
    ACPI device for the EC, [GOOG0004] and EC firmware change that directs
    the MKBP events over ACPI.
    
    BUG=b:27849483
    TEST=Using scripts, check that accel event on Cyan are sent to the
    kernel using the ring interface.
    
    Move changes in cros_ec_lpc.c from mfd to platform/chrome.
    
    Change-Id: Ia0238204a1f4ff049fb2399da093c08d853dc91e
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    (cherry picked from commit 3009c1db817e4f79b2aadb64d4ee4891987c8707)
    Reviewed-on: https://chromium-review.googlesource.com/358155
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    
    Conflicts:
    	drivers/platform/chrome/cros_ec_lpc.c
    	include/linux/mfd/cros_ec.h
    
    [rebase49(groeck): Resolved conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -337,8 +337,9 @@ extern struct attribute_group cros_usb_pd_charger_attr_group;
 /**
  * cros_ec_get_host_event - Return a mask of event set by the EC.
  *
- * When MKBP is supported, when the EC raises an interrupt,
- * We collect the events raised and call the functions in the ec notifier.
+ * Once cros_ec_get_next_event() has been called, if the event source is
+ * a host event, this function returns the precise event that triggered
+ * the interrupt.
  *
  * This function is a helper to know which events are raised.
  */
