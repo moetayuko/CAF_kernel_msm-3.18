a671db609b5bfc1a83acdf7c71c6549f351154e9
Xishi Qiu <qiuxishi@huawei.com>
2016-07-23 22:36:29 +1000

    mem-hotplug: use GFP_HIGHUSER_MOVABLE in, alloc_migrate_target()
    alloc_migrate_target() is called from migrate_pages(), and the page is
    always from user space, so we can add __GFP_HIGHMEM directly.
    
    Link: http://lkml.kernel.org/r/57884EAA.9030603@huawei.com
    Signed-off-by: Xishi Qiu <qiuxishi@huawei.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_isolation.c
+++ mm/page_isolation.c
@@ -283,8 +283,6 @@ int test_pages_isolated(unsigned long start_pfn, unsigned long end_pfn,
 struct page *alloc_migrate_target(struct page *page, unsigned long private,
 				  int **resultp)
 {
-	gfp_t gfp_mask = GFP_USER | __GFP_MOVABLE;
-
 	/*
 	 * TODO: allocate a destination hugepage from a nearest neighbor node,
 	 * accordance with memory policy of the user process if possible. For
@@ -294,9 +292,6 @@ struct page *alloc_migrate_target(struct page *page, unsigned long private,
 		return alloc_huge_page_node(page_hstate(compound_head(page)),
 					    next_node_in(page_to_nid(page),
 							 node_online_map));
-
-	if (PageHighMem(page))
-		gfp_mask |= __GFP_HIGHMEM;
-
-	return alloc_page(gfp_mask);
+	else
+		return alloc_page(GFP_HIGHUSER_MOVABLE);
 }
