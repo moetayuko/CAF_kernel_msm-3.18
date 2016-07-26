e9fd310d6e5840d2370a6ca28437aa8aecd28975
zhong jiang <zhongjiang@huawei.com>
2016-07-27 09:32:58 +1000

    kexec: add restriction on kexec_load() segment sizes
    I hit the following issue when run trinity in my system.  The kernel is
    3.4 version, but mainline has the same issue.
    
    The root cause is that the segment size is too large so the kerenl spends
    too long trying to allocate a page.  Other cases will block until the test
    case quits.  Also, OOM conditions will occur.
    
    Call Trace:
     [<ffffffff81106eac>] __alloc_pages_nodemask+0x14c/0x8f0
     [<ffffffff8124c2be>] ? trace_hardirqs_on_thunk+0x3a/0x3c
     [<ffffffff8124c2be>] ? trace_hardirqs_on_thunk+0x3a/0x3c
     [<ffffffff8124c2be>] ? trace_hardirqs_on_thunk+0x3a/0x3c
     [<ffffffff8124c2be>] ? trace_hardirqs_on_thunk+0x3a/0x3c
     [<ffffffff8124c2be>] ? trace_hardirqs_on_thunk+0x3a/0x3c
     [<ffffffff8113e5ef>] alloc_pages_current+0xaf/0x120
     [<ffffffff810a0da0>] kimage_alloc_pages+0x10/0x60
     [<ffffffff810a15ad>] kimage_alloc_control_pages+0x5d/0x270
     [<ffffffff81027e85>] machine_kexec_prepare+0xe5/0x6c0
     [<ffffffff810a0d52>] ? kimage_free_page_list+0x52/0x70
     [<ffffffff810a1921>] sys_kexec_load+0x141/0x600
     [<ffffffff8115e6b0>] ? vfs_write+0x100/0x180
     [<ffffffff8145fbd9>] system_call_fastpath+0x16/0x1b
    
    The patch chnages sanity_check_segment_list() to verify that no segment is
    larger than half of memory.
    
    Link: http://lkml.kernel.org/r/1469165782-13193-1-git-send-email-zhongjiang@huawei.com
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kexec_core.c
+++ kernel/kexec_core.c
@@ -154,6 +154,7 @@ static struct page *kimage_alloc_page(struct kimage *image,
 int sanity_check_segment_list(struct kimage *image)
 {
 	int i;
+	unsigned long total_segments = 0;
 	unsigned long nr_segments = image->nr_segments;
 
 	/*
@@ -213,6 +214,21 @@ int sanity_check_segment_list(struct kimage *image)
 		if (image->segment[i].bufsz > image->segment[i].memsz)
 			return -EINVAL;
 	}
+
+	/* Verity all segment size donnot exceed the specified size.
+	 * if segment size from user space is too large,  a large
+	 * amount of time will be wasted when allocating page. so,
+	 * softlockup may be come up.
+	 */
+	for (i = 0; i < nr_segments; i++) {
+		if (image->segment[i].memsz > (totalram_pages / 2))
+			return -EINVAL;
+
+		total_segments += image->segment[i].memsz;
+	}
+
+	if (total_segments > (totalram_pages / 2))
+		return -EINVAL;
 
 	/*
 	 * Verify we have good destination addresses.  Normally
