c947d7fda80524d0477441559be6bf4ebec6c2ae
Shawn Nematbakhsh <shawnn@chromium.org>
2016-08-17 18:11:09 -0700

    CHROMIUM: cros_ec: Don't signal wake event for non-wake host events
    The subset of wake-enabled host events is defined by the EC, but the EC
    may still send non-wake host events if we're in the process of
    suspending. Get the mask of wake-enabled host events from the EC and
    filter out non-wake events to prevent spurious aborted suspend
    attempts.
    
    BUG=chrome-os-partner:56156
    BRANCH=None
    TEST=On kevin with corresponding EC change. Verify that lid open, lid
    close and power button host events are marked as wake-enabled. Attach
    charger and verify that various battery / charging host events are not
    marked as wake-enabled.
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I4d28f1941e650fe3198bf3ae91c7e873b5c1f980
    Reviewed-on: https://chromium-review.googlesource.com/372399
    Commit-Ready: Douglas Anderson <dianders@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    
    Conflicts:
    	drivers/mfd/cros_ec.c
    	drivers/platform/chrome/cros_ec_proto.c
    
    [rebase49(groeck): Resolved conflicts;
    	mkpb event support is assumed to always be available upstream]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -172,6 +172,7 @@ struct cros_ec_device {
 
 	struct ec_response_get_next_event event_data;
 	int event_size;
+	u32 host_event_wake_mask;
 };
 
 /* struct cros_ec_dev_platform - ChromeOS EC platform information
