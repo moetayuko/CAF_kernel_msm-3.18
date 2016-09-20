c2921c559ec50dbf2b2701e9dedbf05f6f327041
Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
2016-09-21 09:45:58 +1000

    powerpc: kexec_file: add buffer hand-over support for the next kernel
    The buffer hand-over mechanism allows the currently running kernel to pass
    data to kernel that will be kexec'd via a kexec segment.  The second
    kernel can check whether the previous kernel sent data and retrieve it.
    
    This is the architecture-specific part.
    
    Link: http://lkml.kernel.org/r/1473900890-1476-4-git-send-email-bauerman@linux.vnet.ibm.com
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Stewart Smith <stewart@linux.vnet.ibm.com>
    Cc: Mimi Zohar <zohar@linux.vnet.ibm.com>
    Cc: Eric Richter <erichte@linux.vnet.ibm.com>
    Cc: Balbir Singh <bsingharora@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/powerpc/include/asm/kexec.h
+++ arch/powerpc/include/asm/kexec.h
@@ -92,12 +92,20 @@ static inline bool kdump_in_progress(void)
 }
 
 #ifdef CONFIG_KEXEC_FILE
+#define ARCH_HAS_KIMAGE_ARCH
+
+struct kimage_arch {
+	phys_addr_t handover_buffer_addr;
+	unsigned long handover_buffer_size;
+};
+
 int setup_purgatory(struct kimage *image, const void *slave_code,
 		    const void *fdt, unsigned long kernel_load_addr,
 		    unsigned long fdt_load_addr, unsigned long stack_top,
 		    int debug);
-int setup_new_fdt(void *fdt, unsigned long initrd_load_addr,
-		  unsigned long initrd_len, const char *cmdline);
+int setup_new_fdt(const struct kimage *image, void *fdt,
+		  unsigned long initrd_load_addr, unsigned long initrd_len,
+		  const char *cmdline);
 bool find_debug_console(const void *fdt);
 #endif /* CONFIG_KEXEC_FILE */
 
