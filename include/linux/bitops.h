797e756e9f3fcd1240fe210979659a4b30def010
Andrew Morton <akpm@linux-foundation.org>
2016-10-05 09:14:18 +1100

    mm-vmalloc-fix-align-value-calculation-error-v2-fix-fix
    move get_count_order[_long] definitions to pick up fls_long()
    
    Cc: zijun_hu <zijun_hu@htc.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/bitops.h
+++ include/linux/bitops.h
@@ -259,6 +259,32 @@ static inline unsigned long __ffs64(u64 word)
 })
 #endif
 
+static inline int get_count_order(unsigned int count)
+{
+	int order;
+
+	order = fls(count) - 1;
+	if (count & (count - 1))
+		order++;
+	return order;
+}
+
+/**
+ * get_count_order_long - get order after rounding @l up to power of 2
+ * @l: parameter
+ *
+ * it is same as get_count_order() but with long type parameter
+ */
+static inline int get_count_order_long(unsigned long l)
+{
+	if (l == 0UL)
+		return -1;
+	else if (l & (l - 1UL))
+		return (int)fls_long(l);
+	else
+		return (int)fls_long(l) - 1;
+}
+
 #ifndef find_last_bit
 /**
  * find_last_bit - find the last set bit in a memory region
