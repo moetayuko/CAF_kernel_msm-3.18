cb038527cd48d21a25b3fdb705c20d6401ffd53f
Andrey Pronin <apronin@chromium.org>
2017-01-13 10:26:38 -0800

    CHROMIUM: tpm: move shutdown logic from common code
    The shutdown logic is moved from the common gentle shutdown notifier to
    device-specific .shutdown handlers. This is done because the current
    shutdown notifier prevents sending any commands to tpm after it is
    called, and we need to send vendor-specific commands during shutdown.
    
    The new shutdown handlers still prevent sending further commands to
    the tpm after they are called. But the method is different:
    Instead of locking and never releasing chip->tpm_mutex, we now rely
    on the standard tpm_chip_unregister(), which among other things
    (1) internally acquires chip->ops_sem and clears out chip->ops,
    (2) unregisters sysfs entries.
    
    The common tpm driver code already makes sure that all calls from its
    external interface (through /dev/tpm0 or through exported symbols like
    tpm_get_random()) go through chip->ops while holding chip_ops_sem.
    Except for sysfs, but those sysfs entries are also unregistered in
    tpm_chip_unregister().
    
    So, calling tpm_chip_unregister() makes sure there are no incomplete
    tpm transfers, and commands can't be sent to the tpm in the future.
    Thus, a tpm, for which tpm_chip_unregister() is called in its shutdown
    handler, can't be then reset in the middle of processing a command
    and go into the defensive lockout state as a result.
    
    To satisfy the tests:
    "[gentle shutdown]" printout is removed from the common code that no
    longer guarantees it. And added to individual drivers, which call
    tpm_chip_unregister() from their shutdown handlers.
    
    BUG=chrome-os-partner:59007
    TEST=shutdown on devices with tpm 1.2 and tpm 2.0 running 4.4 kernel,
         verify that the shutdown handler is called.
    
    Change-Id: I292e23f659831296e9d0b0d3f16af3bc2ce536cd
    Signed-off-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/428295
    Reviewed-by: Duncan Laurie <dlaurie@google.com>
    
    Conflicts:
    	drivers/char/tpm/tpm-chip.c
    
    [rebase49(groeck): Fixed context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/char/tpm/tpm-chip.c
+++ drivers/char/tpm/tpm-chip.c
@@ -25,7 +25,6 @@
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/freezer.h>
-#include <linux/reboot.h>
 #include <linux/major.h>
 #include "tpm.h"
 #include "tpm_eventlog.h"
@@ -123,8 +122,6 @@ struct tpm_chip *tpm_chip_find_get(int chip_num)
 static void tpm_dev_release(struct device *dev)
 {
 	struct tpm_chip *chip = container_of(dev, struct tpm_chip, dev);
-
-	unregister_reboot_notifier(&chip->shutdown_nb);
 
 	mutex_lock(&idr_lock);
 	idr_remove(&dev_nums_idr, chip->dev_num);
@@ -342,18 +339,6 @@ static int tpm_add_legacy_sysfs(struct tpm_chip *chip)
 	return 0;
 }
 
-static int tpm_shutdown_notify(struct notifier_block *nb,
-				unsigned long unused, void *unused2)
-{
-	struct tpm_chip *chip = container_of(nb, struct tpm_chip, shutdown_nb);
-	if (chip->flags & TPM_CHIP_FLAG_TPM2)
-		tpm2_shutdown(chip, TPM2_SU_CLEAR);
-	dev_dbg(&chip->dev, "acquiring shutdown lock\n");
-	mutex_lock(&chip->tpm_mutex);
-	dev_dbg(&chip->dev, "acquired shutdown lock\n");
-	return NOTIFY_DONE;
-}
-
 /*
  * tpm_chip_register() - create a character device for the TPM chip
  * @chip: TPM chip to use.
@@ -391,14 +376,6 @@ int tpm_chip_register(struct tpm_chip *chip)
 		tpm1_chip_unregister(chip);
 		return rc;
 	}
-
-	/* INFINEON TPM WORKAROUND: Register shutdown callback that ensures we
-	 * don't shut down in the middle of a TPM command, or we may trigger
-	 * nasty defensive timeouts at the next boot.
-	 */
-	chip->shutdown_nb.notifier_call = tpm_shutdown_notify;
-	dev_info(&chip->dev, "registering reboot notifier [gentle shutdown]\n");
-	register_reboot_notifier(&chip->shutdown_nb);
 
 	chip->flags |= TPM_CHIP_FLAG_REGISTERED;
 
