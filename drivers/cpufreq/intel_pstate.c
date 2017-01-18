42016baef034e2ea25e86acfc16e4f9ff0bbba15
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2017-01-18 10:48:24 -0800

    UPSTREAM: cpufreq: intel_pstate: Make HWP limits compatible with legacy
    Under HWP the performance limits are calculated using max_perf_pct
    and min_perf_pct using possible performance, not available performance.
    The available performance can be reduced by no_turbo setting. To make
    compatible with legacy mode, use max/min performance percentage with
    respect to available performance.
    
    BUG=chrome-os-partner:61059
    TEST=cd /sys/devices/system/cpu -> sysfs entry for intel_pstate/
    
    Change-Id: Ie09a68d8a91cfeaaa5507a292d65a1cc2b427050
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit 4e5d3f713b61)

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -567,7 +567,10 @@ static void intel_pstate_hwp_set(const struct cpumask *cpumask)
 	for_each_cpu(cpu, cpumask) {
 		rdmsrl_on_cpu(cpu, MSR_HWP_CAPABILITIES, &cap);
 		hw_min = HWP_LOWEST_PERF(cap);
-		hw_max = HWP_HIGHEST_PERF(cap);
+		if (limits->no_turbo)
+			hw_max = HWP_GUARANTEED_PERF(cap);
+		else
+			hw_max = HWP_HIGHEST_PERF(cap);
 		range = hw_max - hw_min;
 
 		rdmsrl_on_cpu(cpu, MSR_HWP_REQUEST, &value);
@@ -578,11 +581,6 @@ static void intel_pstate_hwp_set(const struct cpumask *cpumask)
 
 		adj_range = limits->max_perf_pct * range / 100;
 		max = hw_min + adj_range;
-		if (limits->no_turbo) {
-			hw_max = HWP_GUARANTEED_PERF(cap);
-			if (hw_max < max)
-				max = hw_max;
-		}
 
 		value &= ~HWP_MAX_PERF(~0L);
 		value |= HWP_MAX_PERF(max);
