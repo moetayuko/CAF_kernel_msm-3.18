127b95da8efedf302bc7d8af142a627d38812712
yusukes <yusukes@google.com>
2017-03-13 17:53:54 -0700

    CHROMIUM: alt-syscall: Whitelist getcpu()
    Android's bionic libc uses the system call to implement
    sched_getcpu(3), and sched_getcpu() is exported to both the
    framework and apps.
    
    Since Bionic always calls getcpu() with node == NULL and
    tcache == NULL, returns EPERM when either of them is not
    NULL.
    
    BUG=b:36139813
    TEST=start ARC, ssh caroline 'dmesg | grep blocked'
    
    Change-Id: I78ae10ae327f474d25587d82f15198744a7a7d90
    Reviewed-on: https://chromium-review.googlesource.com/455219
    Commit-Ready: Yusuke Sato <yusukes@chromium.org>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2475,6 +2475,7 @@ SYSCALL_DEFINE3(getcpu, unsigned __user *, cpup, unsigned __user *, nodep,
 		err |= put_user(cpu_to_node(cpu), nodep);
 	return err ? -EFAULT : 0;
 }
+EXPORT_SYMBOL(sys_getcpu);
 
 /**
  * do_sysinfo - fill in sysinfo struct
