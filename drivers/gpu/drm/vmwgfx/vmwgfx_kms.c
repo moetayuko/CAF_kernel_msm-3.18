899bf04dd41cbf738799870f263d92a3e06a0c84
Daniel Vetter <daniel.vetter@ffwll.ch>
2017-04-03 10:32:50 +0200

    BACKPORT: drm: Make drm_modeset_lock_crtc internal
    This is only for legacy paths that need to grab the crtc/plane lock
    combo. If you want to lock a crtc, just use drm_modeset_lock().
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170403083304.9083-2-daniel.vetter@ffwll.ch
    
    (cherry picked from drm-intel-next-queued 5f27502655f657989dac0622ecf70b9e173e90f8)
    
    Forklift Note: Added fix-up hunks for drivers/gpu/drm/tilcdc/tilcdc_crtc.c
    and drivers/gpu/drm/vmwgfx/vmwgfx_kms.c, as they weres still calling
    drm_modeset_lock_crtc and drm_modeset_unlock_crtc

--- drivers/gpu/drm/vmwgfx/vmwgfx_kms.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_kms.c
@@ -155,7 +155,7 @@ int vmw_du_crtc_cursor_set2(struct drm_crtc *crtc, struct drm_file *file_priv,
 	 * can do this since the caller in the drm core doesn't check anything
 	 * which is protected by any looks.
 	 */
-	drm_modeset_unlock_crtc(crtc);
+	drm_modeset_unlock(&crtc->cursor->mutex);
 	drm_modeset_lock_all(dev_priv->dev);
 	hotspot_x = hot_x + du->hotspot_x;
 	hotspot_y = hot_y + du->hotspot_y;
@@ -225,7 +225,7 @@ int vmw_du_crtc_cursor_set2(struct drm_crtc *crtc, struct drm_file *file_priv,
 
 out:
 	drm_modeset_unlock_all(dev_priv->dev);
-	drm_modeset_lock_crtc(crtc, crtc->cursor);
+	drm_modeset_lock(&crtc->cursor->mutex, NULL);
 
 	return ret;
 }
@@ -246,7 +246,7 @@ int vmw_du_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 	 * can do this since the caller in the drm core doesn't check anything
 	 * which is protected by any looks.
 	 */
-	drm_modeset_unlock_crtc(crtc);
+	drm_modeset_unlock(&crtc->cursor->mutex);
 	drm_modeset_lock_all(dev_priv->dev);
 
 	vmw_cursor_update_position(dev_priv, shown,
@@ -256,7 +256,7 @@ int vmw_du_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 				   du->core_hotspot_y);
 
 	drm_modeset_unlock_all(dev_priv->dev);
-	drm_modeset_lock_crtc(crtc, crtc->cursor);
+	drm_modeset_lock(&crtc->cursor->mutex, NULL);
 
 	return 0;
 }
