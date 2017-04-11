538eaa212a8cbc1775a64aa7a45943cb28ef0689
Paul Mackerras <paulus@ozlabs.org>
2017-04-12 09:17:06 +1000

    KVM: PPC: Book3S PR: Fix build when PR KVM is built in
    Commit f8513214ac9d("KVM: PPC: Book3S: Add MMIO emulation for FP and
    VSX instructions", 2017-02-21) added uses of kvm_cvt_fd() and
    kvm_cvt_df() in arch/powerpc/kvm/powerpc.c, and therefore added
    arch/powerpc/kvm/fpu.S to the list of sources to be included in the
    main KVM module.  However, the PR KVM module also uses fpu.S and has
    it in the list of sources for the PR KVM module.  When PR KVM is built
    in (rather than being compiled as a module), this means we get two
    copies of fpu.o, leading to errors like this:
    
    arch/powerpc/kvm/fpu.o:(.opd+0x0): multiple definition of `fps_fres'
    arch/powerpc/kvm/fpu.o:(.opd+0x0): first defined here
    arch/powerpc/kvm/fpu.o: In function `fps_fres':
    (.text+0x0): multiple definition of `.fps_fres'
    arch/powerpc/kvm/fpu.o:(.text+0x0): first defined here
    arch/powerpc/kvm/fpu.o:(.opd+0x18): multiple definition of
    `fps_frsqrte'
    arch/powerpc/kvm/fpu.o:(.opd+0x18): first defined here
    arch/powerpc/kvm/fpu.o: In function `fps_frsqrte':
    (.text+0x20): multiple definition of `.fps_frsqrte'
    arch/powerpc/kvm/fpu.o:(.text+0x20): first defined here
    ... and so on ...
    
    To fix the problem, this replaces the calls to kvm_cvt_fd() and
    kvm_cvt_df() in powerpc.c with inline asm, meaning that we no longer
    need to include fpu.S in the list of sources for the main KVM module.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- arch/powerpc/kvm/powerpc.c
+++ arch/powerpc/kvm/powerpc.c
@@ -37,7 +37,6 @@
 #include <asm/cputhreads.h>
 #include <asm/irqflags.h>
 #include <asm/iommu.h>
-#include <asm/kvm_fpu.h>
 #include <asm/switch_to.h>
 #include "timing.h"
 #include "irq.h"
@@ -910,7 +909,8 @@ static inline u64 sp_to_dp(u32 fprs)
 
 	preempt_disable();
 	enable_kernel_fp();
-	kvm_cvt_fd(&fprs, &fprd);
+	asm ("lfs%U1%X1 0,%1; stfd%U0%X0 0,%0" : "=m" (fprd) : "m" (fprs)
+	     : "fr0");
 	preempt_enable();
 	return fprd;
 }
@@ -921,7 +921,8 @@ static inline u32 dp_to_sp(u64 fprd)
 
 	preempt_disable();
 	enable_kernel_fp();
-	kvm_cvt_df(&fprd, &fprs);
+	asm ("lfd%U1%X1 0,%1; stfs%U0%X0 0,%0" : "=m" (fprs) : "m" (fprd)
+	     : "fr0");
 	preempt_enable();
 	return fprs;
 }
