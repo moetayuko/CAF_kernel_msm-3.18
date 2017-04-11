jcadduono/android_kernel_google_chromeos

CHROMIUM: cros_ec: Fix module loading issues.
Name of the driver in the C code was not matching their Makefile names.
This prevented the drivers from loaded as module.
Also, export _mec entry points to allow the lpc module to use these
function.

BUG=none
TEST=Load when compiled as module.

Change-Id: I583cdc4afc5d406a3e4ab7e0ddc5caff1b42ff24
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/341673
Reviewed-by: Guenter Roeck <groeck@chromium.org>

drivers/mfd/cros_ec_lpc.c
@@ -29,7 +29,7 @@
 #include <linux/platform_device.h>
 #include <linux/printk.h>
 
-#define MYNAME "cros_ec_lpc"
+#define MYNAME "cros_ec_lpcs"
 
 static int ec_response_timed_out(void)
 {

moetayuko/CAF_kernel_msm-3.18

Conflicts:
	drivers/mfd/cros_ec_dev.c
	drivers/mfd/cros_ec_lpc.c
(cherry picked from commit 78681ac0a5437a28c2e91bcfdcdb9d4c40d55cf8)
Signed-off-by: Guenter Roeck <groeck@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/355523
	drivers/platform/chrome/Makefile
[rebase49(groeck): Context conflicts]

drivers/platform/chrome/cros_ec_lpc.c
@@ -31,7 +31,7 @@
 #include <linux/platform_device.h>
 #include <linux/printk.h>
 
-#define DRV_NAME "cros_ec_lpc"
+#define DRV_NAME "cros_ec_lpcs"
 
 static int ec_response_timed_out(void)
 {