18d3e9efb4e7b17e34f81267630df3d577e8314b
Douglas Anderson <dianders@chromium.org>
2017-05-31 09:02:25 -0700

    CHROMIUM: cros_ec: Don't leak memory when setting keyboard wake angle
    We allocated msg, but didn't free it.  Free it.
    
    BUG=b:36464431
    TEST=Mess w/ /sys/devices/.../cros_ec/kb_wake_angle and kmemleak is silent
    
    Change-Id: I7e44c88f56c1bfe9451cf0e809e51287ef772760
    Fixes: 46e234872a54 ("CHROMIUM: cros_ec: Add sysfs entry to set keyboard wake angle.")
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/519603
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/platform/chrome/cros_ec_sysfs.c
+++ drivers/platform/chrome/cros_ec_sysfs.c
@@ -285,10 +285,13 @@ static ssize_t show_kb_wake_angle(struct device *dev,
 	msg->insize = sizeof(*resp);
 	ret = cros_ec_cmd_xfer_status(ec->ec_dev, msg);
 	if (ret < 0)
-		return ret;
+		goto exit;
 	resp = (struct ec_response_motion_sense *)msg->data;
-	return scnprintf(buf, PAGE_SIZE, "%d\n",
+	ret = scnprintf(buf, PAGE_SIZE, "%d\n",
 			resp->kb_wake_angle.ret);
+exit:
+	kfree(msg);
+	return ret;
 }
 
 static ssize_t store_kb_wake_angle(struct device *dev,
@@ -318,6 +321,7 @@ static ssize_t store_kb_wake_angle(struct device *dev,
 	msg->outsize = sizeof(*param);
 	msg->insize = sizeof(struct ec_response_motion_sense);
 	ret = cros_ec_cmd_xfer_status(ec->ec_dev, msg);
+	kfree(msg);
 	if (ret < 0)
 		return ret;
 	return count;
