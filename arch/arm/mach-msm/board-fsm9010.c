98ab4020deeeec19f5813e1b01704c37bc4ef9d8
Yasir Malik <ymalik@codeaurora.org>
2017-06-01 12:45:14 -0400

    msm: fsm9010: add support for flexible memory map
    FSM9010 is enabling flexible memory map for LTE L2 DL/UL regions. Read
    the region address and size from memory map table in IMEM and populate
    the dl/ul-bufs properties in danipc DT node.
    
    CRs-Fixed: 2047326
    Change-Id: I679d6efa2600374c69f1487abc5f99800b7012b2
    Acked-by: Jinesh Kunnathully Jayakumar <jineshk@qti.qualcomm.com>
    Signed-off-by: Yasir Malik <ymalik@codeaurora.org>

--- arch/arm/mach-msm/board-fsm9010.c
+++ arch/arm/mach-msm/board-fsm9010.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -47,6 +47,62 @@
 #define FSM9010_NSS_TCM_PHYS		0xe4000000
 
 #define FSM9010_UIO_VERSION "1.0"
+
+#define FSM9010_MEM_MAP_PHYS             0xFE800400
+#define FSM9010_MEM_MAP_SIZE             0x400
+
+#define FSM9010_MEM_TAG_NONE                 0x00000000
+#define FSM9010_MEM_TAG_OEM_DEBUG            0x00000001
+#define FSM9010_MEM_TAG_LOADABLE_HEX0        0x00000002
+#define FSM9010_MEM_TAG_LOADABLE_HEX1        0x00000003
+#define FSM9010_MEM_TAG_LOADABLE_HEX2        0x00000004
+#define FSM9010_MEM_TAG_LOADABLE_HEX3        0x00000005
+#define FSM9010_MEM_TAG_SHARED_LTEFAPI_UL    0x00000006
+#define FSM9010_MEM_TAG_SHARED_LTEFAPI_DL    0x00000007
+#define FSM9010_MEM_TAG_SHARED_LTEIPC        0x00000008
+#define FSM9010_MEM_TAG_SHARED_LTEL2_DL      0x00000009
+#define FSM9010_MEM_TAG_SHARED_LTEL2_UL      0x0000000A
+#define FSM9010_MEM_TAG_LOADABLE_UBI         0x0000000B
+
+#define VMID_NOACCESS			0
+#define VMID_RPM			1
+#define VMID_TZ				2
+#define VMID_AP				3
+#define VMID_HEX_0			4
+#define VMID_HEX_1			5
+#define VMID_HEX_2			6
+#define VMID_HEX_3			7
+#define VMID_CTTHRT			8
+#define VMID_SCLTE			9
+#define VMID_NAV			10
+#define VMID_EMAC0			11
+#define VMID_EMAC1			12
+#define VMID_PCIE0			13
+#define VMID_PCIE1			14
+
+#define VMID_NOACCESS_BIT		(1<<VMID_NOACCESS)
+#define VMID_RPM_BIT			(1<<VMID_RPM)
+#define VMID_TZ_BIT			(1<<VMID_TZ)
+#define VMID_AP_BIT			(1<<VMID_AP)
+#define VMID_HEX_0_BIT			(1<<VMID_HEX_0)
+#define VMID_HEX_1_BIT			(1<<VMID_HEX_1)
+#define VMID_HEX_2_BIT			(1<<VMID_HEX_2)
+#define VMID_HEX_3_BIT			(1<<VMID_HEX_3)
+#define VMID_CTTHRT_BIT			(1<<VMID_CTTHRT)
+#define VMID_SCLTE_BIT			(1<<VMID_SCLTE)
+#define VMID_NAV_BIT			(1<<VMID_NAV)
+#define VMID_EMAC0_BIT			(1<<VMID_EMAC0)
+#define VMID_EMAC1_BIT			(1<<VMID_EMAC1)
+#define VMID_PCIE0_BIT			(1<<VMID_PCIE0)
+#define VMID_PCIE1_BIT			(1<<VMID_PCIE1)
+
+struct mem_map_seg {
+	u32 phy_addr;
+	u32 sz;
+	u32 rd_vmid;
+	u32 wr_vmid;
+	u32 tag;
+} __attribute__((__packed__));
 
 static struct uio_info fsm9010_uio_info[] = {
 	{
@@ -188,9 +244,26 @@ static struct platform_device *fsm9010_uio_devices[] = {
 
 static const char mac_addr_prop_name[] = "mac-address";
 static const char mac_addr_prop_name1[] = "local-mac-address";
+static const char shm_ul_bufs_prop_name[] = "ul-bufs";
+static const char shm_dl_bufs_prop_name[] = "dl-bufs";
 
 void __init fsm9010_reserve(void)
 {
+}
+
+static struct mem_map_seg *find_mem_map_seg(struct mem_map_seg *tbl, u32 tag)
+{
+	if (tbl == NULL)
+		return NULL;
+
+	while (tbl->tag != FSM9010_MEM_TAG_NONE) {
+		if (tbl->tag == tag)
+			return tbl;
+
+		tbl++;
+	}
+
+	return NULL;
 }
 
 /*
@@ -319,6 +392,102 @@ int __init fsm9010_gmac_dt_update(void)
 	return 0;
 }
 
+static int add_danipc_property(struct device_node *np,
+			       struct mem_map_seg *region,
+			       const char *name)
+{
+	u32 buf[2];
+	struct property *pbuf = NULL;
+
+	pr_info("%s: adding danipc DT prop %s=<0x%x 0x%x>\n",
+		__func__, name, region->phy_addr, region->sz);
+
+	if (!(region->rd_vmid & VMID_AP_BIT) ||
+	    !(region->wr_vmid & VMID_AP_BIT)) {
+		pr_err("do not have permissions for %s\n", name);
+		return -EPERM;
+	}
+
+	buf[0] = region->phy_addr;
+	buf[1] = region->sz;
+
+	pbuf = kzalloc(sizeof(*pbuf) + sizeof(buf), GFP_KERNEL);
+
+	if (pbuf == NULL)
+		return -ENOMEM;
+
+	pbuf->value = pbuf + 1;
+	pbuf->length = sizeof(buf);
+	pbuf->name = (char *)name;
+	memcpy(pbuf->value, buf, sizeof(buf));
+
+	of_add_property(np, pbuf);
+
+	return 0;
+}
+
+static int __init fsm9010_ipc_buf_region_update(void)
+{
+	struct device_node *np = NULL;
+	int ret = -ENODEV;
+	void *mem_map_region = NULL;
+	struct mem_map_seg __iomem *mem_map_table;
+	struct mem_map_seg *ul_region;
+	struct mem_map_seg *dl_region;
+
+	/* We expect only a single danipc device node */
+	np = of_find_compatible_node(NULL, NULL, "qcom,danipc");
+
+	if (np == NULL) {
+		pr_err("failed to find dt node for qcom,danipc\n");
+		return -ENODEV;
+	}
+
+	mem_map_region = ioremap(FSM9010_MEM_MAP_PHYS, FSM9010_MEM_MAP_SIZE);
+
+	if (mem_map_region == NULL) {
+		pr_err("failed to map memory map");
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	/* Version number comes first */
+	mem_map_table = mem_map_region + sizeof(u32);
+
+	/* Find the UL and DL regions and store them in the DT */
+	ul_region = find_mem_map_seg(mem_map_table,
+			FSM9010_MEM_TAG_SHARED_LTEL2_UL);
+
+	dl_region = find_mem_map_seg(mem_map_table,
+			FSM9010_MEM_TAG_SHARED_LTEL2_DL);
+
+	if (dl_region == NULL || ul_region == NULL) {
+		pr_err("could not find regions: ul=%pK, dl=%pK",
+			ul_region, dl_region);
+		ret = -ENODEV;
+		goto out;
+	}
+
+	ret = add_danipc_property(np, dl_region, shm_dl_bufs_prop_name);
+
+	if (ret != 0)
+		goto out;
+
+	ret = add_danipc_property(np, ul_region, shm_ul_bufs_prop_name);
+
+	if (ret != 0)
+		goto out;
+
+out:
+	of_node_put(np);
+
+	if (mem_map_region)
+		iounmap(mem_map_region);
+
+	return ret;
+}
+
+
 void __init fsm9010_init(void)
 {
 	/*
@@ -334,6 +503,7 @@ void __init fsm9010_init(void)
 		pr_err("%s: socinfo_init() failed\n", __func__);
 
 	fsm9010_gmac_dt_update();
+	fsm9010_ipc_buf_region_update();
 
 	fsm9010_add_drivers();
 }
