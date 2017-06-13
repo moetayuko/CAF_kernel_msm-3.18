e1e575e5a84cdfb2694233a84c17793f756a51dc
Yasir Malik <ymalik@codeaurora.org>
2017-06-13 10:42:48 -0400

    msm: fsm9010: update memory map for fsm9010
    FSM9010 memory map is being updated to restucture/resize regions and
    remove ununsed ones for improved space efficiency.
    
    CRs-Fixed: 2047326
    Change-Id: I3f9752c0e0a9ae202298a7da92e085623c2b1b17
    Acked-by: Jinesh Kunnathully Jayakumar <jineshk@qti.qualcomm.com>
    Signed-off-by: Yasir Malik <ymalik@codeaurora.org>

--- arch/arm/mach-msm/board-fsm9010.c
+++ arch/arm/mach-msm/board-fsm9010.c
@@ -32,19 +32,36 @@
 #define FSM9010_MAC1_FUSE_PHYS	0xFC4B8448
 #define FSM9010_MAC_FUSE_SIZE	0x10
 
-#define FSM9010_SCLTE_DDR_PHYS		0x00100000
-#define FSM9010_SCLTE_NL_SAMPLES_PHYS	0x15000000
-#define FSM9010_CDU_PHYS		0x17000000
-#define FSM9010_SCLTE_TRACE_DEBUG_PHYS	0x18100000
-#define FSM9010_SCLTE_ETH_TRACE_PHYS	0x18180000
-#define FSM9010_SCLTE_DEBUG_DUMP_PHYS	0x18280000
-#define FSM9010_SCLTE_CB_TRACE_PHYS	0x18300000
-#define FSM9010_SCLTE_RF_CAL_PHYS	0x29000000
-#define FSM9010_SCLTE_GEN_DBG_PHYS	0xf5000000
-#define FSM9010_QDSP6_0_DEBUG_DUMP_PHYS	0x18900000
-#define FSM9010_QDSP6_1_DEBUG_DUMP_PHYS	0x18a80000
-#define FSM9010_NSS_CORE_DUMP_PHYS	0x28000000
+#define FSM9010_SCLTE_NL_SAMPLES_PHYS	0x11000000
+#define FSM9010_SCLTE_NL_SAMPLES_SIZE	0x01000000
+
+#define FSM9010_SCLTE_DPD_SAMPLES_PHYS	0x12000000
+#define FSM9010_SCLTE_DPD_SAMPLES_SIZE	0x01000000
+
+#define FSM9010_SCLTE_TRACE_DEBUG_PHYS	0x13100000
+#define FSM9010_SCLTE_TRACE_DEBUG_SIZE	0x00080000
+
+#define FSM9010_SCLTE_DEBUG_DUMP_PHYS	0x13180000
+#define FSM9010_SCLTE_DEBUG_DUMP_SIZE	0x00080000
+
+#define FSM9010_QDSP6_0_DEBUG_DUMP_PHYS	0x13200000
+#define FSM9010_QDSP6_0_DEBUG_DUMP_SIZE	0x00100000
+
+#define FSM9010_QDSP6_1_DEBUG_DUMP_PHYS	0x13300000
+#define FSM9010_QDSP6_1_DEBUG_DUMP_SIZE	0x00100000
+
+/* Used for either DFE waveform data or RF CB Trace */
+#define FSM9010_SCLTE_WF_CB_PHYS	0x13600000
+#define FSM9010_SCLTE_WF_CB_SIZE	0x00a00000
+
+#define FSM9010_NSS_CORE_DUMP_PHYS	0x15000000
+#define FSM9010_NSS_CORE_DUMP_SIZE	0x01000000
+
 #define FSM9010_NSS_TCM_PHYS		0xe4000000
+#define FSM9010_NSS_TCM_SIZE		0x00010000
+
+#define FSM9010_SCLTE_GEN_DBG_PHYS	0xf5000000
+#define FSM9010_SCLTE_GEN_DBG_SIZE	0x03000000
 
 #define FSM9010_UIO_VERSION "1.0"
 
@@ -121,33 +138,24 @@ static struct uio_info fsm9010_uio_info[] = {
 
 static struct resource fsm9010_uio0_resources[] = {
 	{
-		.start = FSM9010_SCLTE_DDR_PHYS,
-		.end   = FSM9010_SCLTE_DDR_PHYS + 59 * SZ_1M - 1,
-		.name  = "sclte_ddr",
-		.flags = IORESOURCE_MEM,
-	},
-	{
 		.start = FSM9010_SCLTE_NL_SAMPLES_PHYS,
-		.end   = FSM9010_SCLTE_NL_SAMPLES_PHYS + SZ_32M - 1,
+		.end   = FSM9010_SCLTE_NL_SAMPLES_PHYS +
+				FSM9010_SCLTE_NL_SAMPLES_SIZE - 1,
 		.name  = "nl_samples",
 		.flags = IORESOURCE_MEM,
 	},
 	{
-		.start = FSM9010_CDU_PHYS,
-		.end   = FSM9010_CDU_PHYS + SZ_16M - 1,
-		.name  = "cdu",
+		.start = FSM9010_SCLTE_DPD_SAMPLES_PHYS,
+		.end   = FSM9010_SCLTE_DPD_SAMPLES_PHYS +
+				FSM9010_SCLTE_DPD_SAMPLES_SIZE - 1,
+		.name  = "dpd_samples",
 		.flags = IORESOURCE_MEM,
 	},
 	{
 		.start = FSM9010_SCLTE_TRACE_DEBUG_PHYS,
-		.end   = FSM9010_SCLTE_TRACE_DEBUG_PHYS + SZ_512K - 1,
+		.end   = FSM9010_SCLTE_TRACE_DEBUG_PHYS +
+				FSM9010_SCLTE_TRACE_DEBUG_SIZE - 1,
 		.name  = "sclte_trace_debug",
-		.flags = IORESOURCE_MEM,
-	},
-	{
-		.start = FSM9010_SCLTE_ETH_TRACE_PHYS,
-		.end   = FSM9010_SCLTE_ETH_TRACE_PHYS + SZ_1M - 1,
-		.name  = "sclte_eth_trace",
 		.flags = IORESOURCE_MEM,
 	},
 };
@@ -165,25 +173,22 @@ static struct platform_device fsm9010_uio0_device = {
 static struct resource fsm9010_uio1_resources[] = {
 	{
 		.start = FSM9010_SCLTE_DEBUG_DUMP_PHYS,
-		.end   = FSM9010_SCLTE_DEBUG_DUMP_PHYS + SZ_512K - 1,
+		.end   = FSM9010_SCLTE_DEBUG_DUMP_PHYS +
+				FSM9010_SCLTE_DEBUG_DUMP_SIZE - 1,
 		.name  = "sclte_debug_dump",
 		.flags = IORESOURCE_MEM,
 	},
 	{
-		.start = FSM9010_SCLTE_CB_TRACE_PHYS,
-		.end   = FSM9010_SCLTE_CB_TRACE_PHYS + SZ_2M - 1,
-		.name  = "sclte_cb_trace",
-		.flags = IORESOURCE_MEM,
-	},
-	{
-		.start = FSM9010_SCLTE_RF_CAL_PHYS,
-		.end   = FSM9010_SCLTE_RF_CAL_PHYS + 10 * SZ_1M - 1,
-		.name  = "sclte_rf_cal",
+		.start = FSM9010_SCLTE_WF_CB_PHYS,
+		.end   = FSM9010_SCLTE_WF_CB_PHYS +
+				FSM9010_SCLTE_WF_CB_SIZE - 1,
+		.name  = "sclte_wf_cb",
 		.flags = IORESOURCE_MEM,
 	},
 	{
 		.start = FSM9010_SCLTE_GEN_DBG_PHYS,
-		.end   = FSM9010_SCLTE_GEN_DBG_PHYS + 48 * SZ_1M - 1,
+		.end   = FSM9010_SCLTE_GEN_DBG_PHYS +
+				FSM9010_SCLTE_GEN_DBG_SIZE - 1,
 		.name  = "sclte_gen_dbg",
 		.flags = IORESOURCE_MEM,
 	},
@@ -202,25 +207,29 @@ static struct platform_device fsm9010_uio1_device = {
 static struct resource fsm9010_uio2_resources[] = {
 	{
 		.start = FSM9010_QDSP6_0_DEBUG_DUMP_PHYS,
-		.end   = FSM9010_QDSP6_0_DEBUG_DUMP_PHYS + SZ_1M + SZ_512K - 1,
+		.end   = FSM9010_QDSP6_0_DEBUG_DUMP_PHYS +
+				FSM9010_QDSP6_0_DEBUG_DUMP_SIZE - 1,
 		.name  = "qdsp6_0_debug_dump",
 		.flags = IORESOURCE_MEM,
 	},
 	{
 		.start = FSM9010_QDSP6_1_DEBUG_DUMP_PHYS,
-		.end   = FSM9010_QDSP6_1_DEBUG_DUMP_PHYS + SZ_1M + SZ_512K - 1,
+		.end   = FSM9010_QDSP6_1_DEBUG_DUMP_PHYS +
+				FSM9010_QDSP6_1_DEBUG_DUMP_SIZE - 1,
 		.name  = "qdsp6_1_debug_dump",
 		.flags = IORESOURCE_MEM,
 	},
 	{
 		.start = FSM9010_NSS_CORE_DUMP_PHYS,
-		.end   = FSM9010_NSS_CORE_DUMP_PHYS + SZ_16M - 1,
+		.end   = FSM9010_NSS_CORE_DUMP_PHYS +
+				FSM9010_NSS_CORE_DUMP_SIZE - 1,
 		.name  = "nss_core_dump",
 		.flags = IORESOURCE_MEM,
 	},
 	{
 		.start = FSM9010_NSS_TCM_PHYS,
-		.end   = FSM9010_NSS_TCM_PHYS + SZ_64K - 1,
+		.end   = FSM9010_NSS_TCM_PHYS +
+				FSM9010_NSS_TCM_SIZE - 1,
 		.name  = "nss_tcm",
 		.flags = IORESOURCE_MEM,
 	},
