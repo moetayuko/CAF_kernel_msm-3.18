59859ee1a6dfe6e6bfe4cb050921a0bb80dbeb49
Stephen Rothwell <sfr@canb.auug.org.au>
2017-08-02 15:55:02 +1000

    mm-thp-enable-thp-migration-in-generic-path-fix-fix
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/swapops.h
+++ include/linux/swapops.h
@@ -291,7 +291,9 @@ static inline swp_entry_t pmd_to_swp_entry(pmd_t pmd)
 
 static inline pmd_t swp_entry_to_pmd(swp_entry_t entry)
 {
-	return (pmd_t){};
+	pmd_t e;
+	memset(&e, 0, sizeof(pmd_t));
+	return e;
 }
 
 static inline int is_pmd_migration_entry(pmd_t pmd)
