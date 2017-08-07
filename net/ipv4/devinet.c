f31b1397d459460d58f0308b40e9a2c971d6bcc1
Guenter Roeck <groeck@chromium.org>
2017-08-07 11:34:45 -0700

    CHROMIUM: Revert "net: socket ioctl to reset connections matching local address"
    This reverts commit 3304747db38251b84649318b38b60d866cbe07b4.
    
    Not needed in Android-N and thus not in chromeos-4.12 either.
    
    BUG=chromium:707244
    TEST=Run Android-N or later
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -60,7 +60,6 @@
 
 #include <net/arp.h>
 #include <net/ip.h>
-#include <net/tcp.h>
 #include <net/route.h>
 #include <net/ip_fib.h>
 #include <net/rtnetlink.h>
@@ -972,7 +971,6 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 	case SIOCSIFBRDADDR:	/* Set the broadcast address */
 	case SIOCSIFDSTADDR:	/* Set the destination address */
 	case SIOCSIFNETMASK: 	/* Set the netmask for the interface */
-	case SIOCKILLADDR:	/* Nuke all sockets on this address */
 		ret = -EPERM;
 		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
 			goto out;
@@ -1024,8 +1022,7 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 	}
 
 	ret = -EADDRNOTAVAIL;
-	if (!ifa && cmd != SIOCSIFADDR && cmd != SIOCSIFFLAGS
-	    && cmd != SIOCKILLADDR)
+	if (!ifa && cmd != SIOCSIFADDR && cmd != SIOCSIFFLAGS)
 		goto done;
 
 	switch (cmd) {
@@ -1151,9 +1148,6 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 			}
 			inet_insert_ifa(ifa);
 		}
-		break;
-	case SIOCKILLADDR:	/* Nuke all connections on this address */
-		ret = tcp_nuke_addr(net, (struct sockaddr *) sin);
 		break;
 	}
 done:
