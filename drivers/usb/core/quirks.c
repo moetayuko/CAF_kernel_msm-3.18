ea568f968ffe76d0fff426a5e77e023f23fcd564
Kai-Heng Feng <kai.heng.feng@canonical.com>
2017-12-20 19:00:08 +0800

    usb: quirks: Add reset-resume quirk for Dell DW1820 QCA Rome Bluetooth
    Commit ("fd865802c66bc451dc515ed89360f84376ce1a56 Bluetooth: btusb: fix
    QCA Rome suspend/resume") enables reset_resume in btusb_probe(). This
    makes the device resets during btusb_open(), firmware loading gets
    interrupted as a result.
    
    We still want to reset the device to solve the original issue, but we
    should do it before btusb_open().
    
    Hence, add reset-resume quirk in usb core intead of btusb.
    
    Cc: stable@vger.kernel.org
    Cc: Leif Liddy <leif.linux@gmail.com>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Cc: Brian Norris <briannorris@chromium.org>
    Cc: Daniel Drake <drake@endlessm.com>
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -197,6 +197,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x0b05, 0x17e0), .driver_info =
 			USB_QUIRK_IGNORE_REMOTE_WAKEUP },
 
+	/* QCA Rome Bluetooth in Dell DW1820 wireless module */
+	{ USB_DEVICE(0x0cf3, 0xe007), .driver_info = USB_QUIRK_RESET_RESUME },
+
 	/* Action Semiconductor flash disk */
 	{ USB_DEVICE(0x10d6, 0x2200), .driver_info =
 			USB_QUIRK_STRING_FETCH_255 },
