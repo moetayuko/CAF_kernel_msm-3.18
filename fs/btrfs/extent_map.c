1e220e1843b7e6392a539de31ca8a1223413b434
Liu Bo <bo.li.liu@oracle.com>
2017-12-21 15:42:56 -0700

    Btrfs: noinline merge_extent_mapping
    In order to debug subtle bugs around merge_extent_mapping(), perf probe
    can be used to check the arguments, but sometimes merge_extent_mapping()
    got inlined by compiler and couldn't be probed.
    
    This is adding noinline attribute to merge_extent_mapping().
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_map.c
+++ fs/btrfs/extent_map.c
@@ -480,10 +480,10 @@ static struct extent_map *prev_extent_map(struct extent_map *em)
  * extent to map_start, and an extent that you want to insert, deal with overlap
  * and insert the best fitted new extent into the tree.
  */
-static int merge_extent_mapping(struct extent_map_tree *em_tree,
-				struct extent_map *existing,
-				struct extent_map *em,
-				u64 map_start, u64 map_len)
+static noinline int merge_extent_mapping(struct extent_map_tree *em_tree,
+					 struct extent_map *existing,
+					 struct extent_map *em,
+					 u64 map_start, u64 map_len)
 {
 	struct extent_map *prev;
 	struct extent_map *next;
