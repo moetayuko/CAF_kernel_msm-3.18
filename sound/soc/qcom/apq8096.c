702cd144ab973e69f2a019bbbb7ab3f7fe26bb5c
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-01-30 11:23:10 +0000

    ASoC: qcom: apq8096: add support to slimbus wcd
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- sound/soc/qcom/apq8096.c
+++ sound/soc/qcom/apq8096.c
@@ -20,6 +20,11 @@
 #include <linux/dma-mapping.h>
 #include <sound/pcm.h>
 
+#include "qdsp6/q6afe.h"
+
+#define SLIM_MAX_RX_PORTS 16
+#define SLIM_MAX_TX_PORTS 16
+
 int msm8996_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 					      struct snd_pcm_hw_params *params)
 {
@@ -33,12 +38,112 @@ int msm8996_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	return 0;
 }
+
+static struct snd_soc_dapm_route wcd9335_audio_paths[] = {
+	{"MIC BIAS1", NULL, "MCLK"},
+	{"MIC BIAS2", NULL, "MCLK"},
+	{"MIC BIAS3", NULL, "MCLK"},
+	{"MIC BIAS4", NULL, "MCLK"},
+};
+
+static int apq8096_audrx_init(struct snd_soc_pcm_runtime *rtd)
+{
+	int err;
+	void *config_data;
+	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(codec);
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	void *mbhc_calibration;
+	struct snd_card *card;
+	struct snd_info_entry *entry;
+	struct msm8996_asoc_mach_data *pdata =
+				snd_soc_card_get_drvdata(rtd->card);
+
+	/* Codec SLIMBUS configuration
+	 * RX1, RX2, RX3, RX4, RX5, RX6, RX7, RX8, RX9, RX10, RX11, RX12, RX13
+	 * TX1, TX2, TX3, TX4, TX5, TX6, TX7, TX8, TX9, TX10, TX11, TX12, TX13
+	 * TX14, TX15, TX16
+	 */
+	unsigned int rx_ch[16] = {144, 145, 146, 147, 148, 149, 150,
+					    151, 152, 153, 154, 155, 156};
+	unsigned int tx_ch[16] = {128, 129, 130, 131, 132, 133,
+					    134, 135, 136, 137, 138, 139,
+					    140, 141, 142, 143};
+
+	snd_soc_dapm_add_routes(dapm, wcd9335_audio_paths,
+				ARRAY_SIZE(wcd9335_audio_paths));
+	snd_soc_dapm_sync(dapm);
+
+	snd_soc_dai_set_channel_map(codec_dai, ARRAY_SIZE(tx_ch),
+				    tx_ch, ARRAY_SIZE(rx_ch), rx_ch);
+
+	return 0;
+out:
+	return err;
+}
+
+static int msm8996_snd_hw_params(struct snd_pcm_substream *substream,
+			     struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	//struct snd_soc_dai_link *dai_link = rtd->dai_link;
+
+	int i, ret = 0;
+	u32 rx_ch[SLIM_MAX_RX_PORTS], tx_ch[SLIM_MAX_TX_PORTS];
+	u32 my_rx_ch[SLIM_MAX_RX_PORTS];
+
+	u32 rx_ch_cnt = 0, tx_ch_cnt = 0;
+	//u32 user_set_tx_ch = 0;
+	u32 rx_ch_count;
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		ret = snd_soc_dai_get_channel_map(codec_dai,
+					&tx_ch_cnt, tx_ch, &rx_ch_cnt , rx_ch);
+		if (ret < 0) {
+			pr_err("%s: failed to get codec chan map, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+
+		my_rx_ch[0] = 0x95;
+		my_rx_ch[1] = 0x96;
+		rx_ch_count = 2;// msm_slim_0_rx_ch;
+		ret = snd_soc_dai_set_channel_map(cpu_dai, 0, 0,
+						  rx_ch_count, my_rx_ch);
+						// rx_ch_count, rx_ch );
+		if (ret < 0) {
+			pr_err("%s: failed to set cpu chan map, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+	}
+end:
+	return ret;
+}
+
+static struct snd_soc_ops msm8996_be_ops = {
+	.hw_params = msm8996_snd_hw_params,
+};
+
 static struct snd_soc_dai_link msm8996_dai_links[] = {
 	/* FrontEnd DAI Links */
 	{
 		.name		= "MultiMedia1 Playback",
 		.stream_name	= "MultiMedia1",
-		.cpu_dai_name	= "MM_DL1",
+		.cpu_dai_name	= "MultiMedia1",
+		.platform_name	= "q6asm_dai",
+		.dynamic	= 1,
+		.dpcm_playback	= 1,
+		.codec_dai_name	= "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+	},
+	{
+		.name		= "MultiMedia2 Playback",
+		.stream_name	= "MultiMedia2",
+		.cpu_dai_name	= "MultiMedia2",
 		.platform_name	= "q6asm_dai",
 		.dynamic	= 1,
 		.dpcm_playback	= 1,
@@ -52,11 +157,26 @@ static struct snd_soc_dai_link msm8996_dai_links[] = {
 		.stream_name	= "q6afe_dai",
 		.cpu_dai_name	= "HDMI",
 		.platform_name	= "q6routing",
+		.id =  AFE_PORT_HDMI_RX,
 		.no_pcm		= 1,
 		.dpcm_playback	= 1,
 		.be_hw_params_fixup = msm8996_be_hw_params_fixup,
 		.codec_dai_name	= "i2s-hifi",
 		.codec_name = "hdmi-audio-codec.0.auto",
+	},
+	{
+		.name		= "Slimbus6 Playback",
+		.stream_name	= "q6afe_dai",
+		.cpu_dai_name	= "SLIMBUS_6_RX",
+		.platform_name	= "q6routing",
+		.id =  SLIMBUS_6_RX,
+		.no_pcm		= 1,
+		.dpcm_playback	= 1,
+		.init = apq8096_audrx_init,
+		.be_hw_params_fixup = msm8996_be_hw_params_fixup,
+		.codec_dai_name	= "wcd9335_rx4",
+		.codec_name = "91c0000.sc:tas:wcd",
+		.ops = &msm8996_be_ops,
 	},
 };
 
